d0863a4f624c587a9695af941b1a91ac26eeee37
Kevin Bauer <kevin.bauer@pointsource.com>
2013-12-07 17:04:00 +0000

    8274 As a BA or Executive, I want to define a project

--- PSMasterMind/app/scripts/controllers/projects/summary.js
+++ PSMasterMind/app/scripts/controllers/projects/summary.js
@@ -4,9 +4,48 @@
  * Controller for handling creation of Roles.
  */
 angular.module('Mastermind.controllers.projects')
-  .controller('SummaryCtrl', ['$scope', 'People',
-    function ($scope, People) {
-      People.query().then(function (people) {
-        $scope.people = people;
+  .controller('SummaryCtrl', ['$scope', '$filter', 'Resources', 'People', 'ngTableParams',
+    function ($scope, $filter, Resources, People, TableParams) {
+	  // Table Parameters
+      var params = {
+        page: 1,            // show first page
+        count: 10,           // count per page
+        sorting: {
+        	type: 'asc'     // initial sorting
+        }
+      };
+      
+      $scope.summaryRolesTableParams = new TableParams(params, {
+        total: $scope.project.roles.length,
+        getData: function ($defer, params) {
+          var start = (params.page() - 1) * params.count();
+          var end = params.page() * params.count();
+
+          var orderedData = params.sorting() ?
+                $filter('orderBy')($scope.project.roles, params.orderBy()) :
+                $scope.project.roles;
+          
+          //use build-in angular filter
+          var ret = orderedData.slice(start, end);
+          
+          var defers = [];
+          
+          for(var i = 0; i < ret.length; i++){
+        	  var ithRole = ret[i];
+        	  if(ithRole.assignee && ithRole.assignee.resource){
+        		  defers.push(Resources.resolve(ithRole.assignee));
+        		  //ithRole.assignee.name = "Test Name " + i + ": " + ithRole.assignee.resource;
+        	  }
+        	  
+        	  if(ithRole.type && ithRole.type.resource){
+        		  defers.push(Resources.resolve(ithRole.type));
+        		  //ithRole.assignee.name = "Test Name " + i + ": " + ithRole.assignee.resource;
+        	  }
+          }
+	
+	      $.when.apply(window, defers).done(function(){
+	    	  $defer.resolve(ret);
+	      });
+        }
       });
     }]);
--- PSMasterMind/app/scripts/services/resources.js
+++ PSMasterMind/app/scripts/services/resources.js
@@ -3,76 +3,110 @@
 /**
  * Get any resource
  */
-angular.module('Mastermind')
-  .factory('Resources', function (Restangular) {
-    var ResourcesRestangular = Restangular.withConfig(function (RestangularConfigurer) {});
+angular.module('Mastermind').factory(
+		'Resources',
+		function(Restangular) {
+			var ResourcesRestangular = Restangular.withConfig(function(
+					RestangularConfigurer) {
+			});
 
-    var Resource = ResourcesRestangular.all('');
-    
-    //Cache Constants
-    var ONE_HOUR = 60 * 60 * 1000; /* ms */
-    var MAX_TIME = ONE_HOUR;
-    var TIME_PREFIX = 'time:';
+			var Resource = ResourcesRestangular.all('');
 
-    /**
-     * Service function for querying a resource member.
-     *
-     * @returns {*}
-     */
-    function query(resource, query, fields, onSuccess) {
-      if(query || fields){
-    	  resource = resource+'?';
-      }	
-    	
-      if(query){
-    	  query = JSON.stringify(query);
-    	  query = encodeURIComponent(query);
-    	  resource = resource+'query='+query;
-      }	
-      
-      if(fields){
-    	  if(query)resource = resource+'&';
-    	  fields = JSON.stringify(fields);
-    	  fields = encodeURIComponent(fields);
-    	  resource = resource+'fields='+fields;
-      }
-    	
-    	
-      return Resource.get(resource).then(onSuccess);
-    }
-    
-    
-    /**
-     * Service function for retrieving a resource member.
-     *
-     * @returns {*}
-     */
-    function get(resource, onSuccess) {
-      //First check if we have this resource in cache
-      var value = localStorage[resource];
-      var time = localStorage[TIME_PREFIX+resource];
-      
-      if(value && time){
-    	   time = Date.parse(time);
-    	   if(((new Date) - time) < MAX_TIME){
-    		   value = JSON.parse(value);
-    		   onSuccess(value);
-    	   }
-      }
-      
-      else{
-    	  return Resource.get(resource).then(function(value){
-    		  //Save to localStorage
-    		  localStorage[resource] = JSON.stringify(value);
-    		  localStorage[TIME_PREFIX+resource] = new Date();
-    		  onSuccess(value);
-    	  }
-    	);
-      }
-    }
+			// Cache Constants
+			var ONE_HOUR = 60 * 60 * 1000; /* ms */
+			var MAX_TIME = ONE_HOUR;
+			var TIME_PREFIX = 'time:';
 
-    return {
-      query: query,
-      get: get
-    };
-  });
+			/**
+			 * Service function for querying a resource member.
+			 * 
+			 * @returns {*}
+			 */
+			function query(resource, query, fields, onSuccess) {
+				if (query || fields) {
+					resource = resource + '?';
+				}
+
+				if (query) {
+					query = JSON.stringify(query);
+					query = encodeURIComponent(query);
+					resource = resource + 'query=' + query;
+				}
+
+				if (fields) {
+					if (query)
+						resource = resource + '&';
+					fields = JSON.stringify(fields);
+					fields = encodeURIComponent(fields);
+					resource = resource + 'fields=' + fields;
+				}
+
+				return Resource.get(resource).then(onSuccess);
+			}
+
+			/**
+			 * Service function for retrieving a resource member.
+			 * 
+			 * @returns {*}
+			 */
+			function get(resource, onSuccess) {
+				// First check if we have this resource in cache
+				var value = localStorage[resource];
+				var time = localStorage[TIME_PREFIX + resource];
+
+				var resolved = false;
+				
+				if (value && time) {
+					time = Date.parse(time);
+					if (((new Date) - time) < MAX_TIME) {
+						value = JSON.parse(value);
+						onSuccess(value);
+						resolved = true;
+					}
+				}
+
+				if(!resolved) {
+					return Resource.get(resource).then(function(value) {
+						// Save to localStorage
+						localStorage[resource] = JSON.stringify(value);
+						localStorage[TIME_PREFIX + resource] = new Date();
+						onSuccess(value);
+						return value;
+					});
+				}
+				else{
+					return value;
+				}
+			}
+			
+			/**
+			 * Resolve a Reference Object
+			 */
+			function resolve(resourceRef){
+				var resource = resourceRef.resource;
+				var deepCopy = function deepCopy(o) {
+			        var copy = o,k;
+			        
+			        if (o && typeof o === 'object') {
+			            copy = Object.prototype.toString.call(o) === '[object Array]' ? [] : {};
+			            for (k in o) {
+			                copy[k] = deepCopy(o[k]);
+			            }
+			        }
+			     
+			        return copy;
+			    };
+			    
+			    return get(resource, function(result){
+			    	for (var k in result) {
+			    		resourceRef[k] = deepCopy(result[k]);
+		            }
+			    });
+			}
+
+			return {
+				query : query,
+				get : get,
+				resolve: resolve
+			};
+		});
