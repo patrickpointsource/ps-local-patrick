0985676163eb29470a3561050cd0a48fa343ef21
Steven Behun <steven.behun@pointsource.com>
2013-12-13 21:03:00 +0000

    changes to app.js for deployment and cleaned up html in roles.html

--- PSMasterMind/app/scripts/app.js
+++ PSMasterMind/app/scripts/app.js
@@ -110,8 +110,17 @@
         });
     })
     .config(function (RestangularProvider) {
+      // local dev
       var serverLocation = 'http://localhost:8080';
+
+      // local dev and stage
+      var restPath = '/MasterMindStaging/rest/';
+
+      // stage and prod
       // var serverLocation = 'http://db.mastermind.pointsource.us:8080';
+
+      // prod only
+      // var restPath = '/MasterMindServer/rest/';
 
       function toJsonReplacer(key, value) {
         var val = value;
@@ -123,7 +132,7 @@
         return val;
       }
 
-      RestangularProvider.setBaseUrl(serverLocation + '/MasterMindStaging/rest/')
+      RestangularProvider.setBaseUrl(serverLocation + restPath)
         .setDefaultHeaders({
           'Authorization': 'Bearer ' + localStorage.getItem('access_token')
         })
