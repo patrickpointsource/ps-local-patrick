9b5d84017e433cf17af83c9ed55a262f42dbe12a
Steven Behun <steven.behun@pointsource.com>
2013-12-16 15:31:00 +0000

    10651 Make labels required field red on on save

--- PSMasterMind/app/scripts/controllers/projects/details.js
+++ PSMasterMind/app/scripts/controllers/projects/details.js
@@ -14,6 +14,15 @@ angular.module('Mastermind.controllers.projects')
 
       $scope.isFieldInError = function (fieldName) {
         var detailsFormField = $scope.detailsForm[fieldName];
+
+        if (fieldName === 'type') {
+          if ($scope.submitAttempted && detailsFormField.$pristine && $scope.project.type === undefined) {
+            return true;
+          }
+        }
+
+        console.log(detailsFormField.$name + ': ');
+        console.log(detailsFormField);
         return (detailsFormField.$dirty
           || ($scope.submitAttempted && detailsFormField.$pristine))
           && detailsFormField.$invalid;
