f10e530a8fd0a035472bb4237bdcba2c2834f920
Kevin Bauer <kevin.bauer@pointsource.com>
2014-01-15 15:27:00 +0000

    14746 When you move backward in time person does not fall off list

--- PSMasterMind/app/scripts/controllers/people/people.js
+++ PSMasterMind/app/scripts/controllers/people/people.js
@@ -100,6 +100,22 @@ angular.module('Mastermind.controllers.people')
         }
         return monthNamesShort[monthNum];
       };
+      
+      /**
+       * display the month name from a month number (0 - 11)
+       */
+      $scope.getShortName = function(date, inc) {
+    	var monthNum = date.getMonth() + inc;
+    	var year = date.getFullYear();
+        if (monthNum > 11) {
+          monthNum = monthNum - 12;
+          year++;
+        }
+        
+        var ret = monthNamesShort[monthNum] + ' ' + year.toString().substring(2);
+        
+        return ret;
+      };
 
       /**
        * build table view
@@ -131,7 +147,7 @@ angular.module('Mastermind.controllers.people')
         var sixMontsFromNowQuery = yyyy6+'-'+mm6+'-'+dd6;
 
         var qvProjQuery = {startDate:{$lte:sixMontsFromNowQuery},$or:[{endDate:{$exists:false}},{endDate:{$gt:startDateQuery}}]};
-        var qvProjFields = {resource:1,name:1,startDate:1,endDate:1,"roles.assignee":1,"roles.rate":1};
+        var qvProjFields = {resource:1,name:1,startDate:1,endDate:1,roles:1};
 
         Resources.query('projects', qvProjQuery, qvProjFields, function(result){
           $scope.qvProjects = result.data;
@@ -147,55 +163,57 @@ angular.module('Mastermind.controllers.people')
           for(var i = 0; i < activeProjects.length; i++){
             var roles = activeProjects[i].roles;
             if(roles){
-            //Arrary to keep track of people already in an accounted role
+              //Arrary to keep track of people already in an accounted role
               var activePeopleProjectsResources = [];
 
               //Loop through all the roles in the active projects
               for(var j = 0; j < roles.length; j++){
                 var activeRole = roles[j];
-
-                //If there is an assignee log it in the active hours
-                if(activeRole.assignee && activeRole.assignee.resource && activeRole.rate){
-                  var hoursPerMonth = 0;
-                  var rate = activeRole.rate;
-                  if(rate.fullyUtilized){
-                    hoursPerMonth = 180;
-                  }
-                  else if(rate.type == 'hourly'){
-                    hoursPerMonth = rate.hours;
-                  }
-                  else if(rate.type == 'weekly'){
-                  // Weekly rate is currently hours per week. There are 5 working days per week
-                      // and 22.5 per month.
-                    var hoursPerMonthNotRounded = parseFloat(rate.hours * 22.5 / 5);
-                    hoursPerMonth = Math.round(hoursPerMonthNotRounded * 100) / 100; // round to 2 decimal places
-                  }
-
-
-                  //If hours are logged from another project increment it
-                  if(activeHours.hasOwnProperty(activeRole.assignee.resource)){
-                    activeHours[activeRole.assignee.resource] += hoursPerMonth;
-                  }
-                  else{
-                    activeHours[activeRole.assignee.resource] = hoursPerMonth;
-                  }
-                }
-
-                if(activeRole.assignee && activeRole.assignee.resource
-                    && !activePeopleProjects.hasOwnProperty(activeRole.assignee.resource)){
-                  //Push the assignnee onto the active list
-                  activePeople.push(Resources.get(activeRole.assignee.resource));
-
-                  //Create a project list
-                  activePeopleProjects[activeRole.assignee.resource] = [activeProjects[i]];
-                  //Accout for person already in role in this project
-                  activePeopleProjectsResources.push(activeRole.assignee.resource);
-                }
-                else if(activeRole.assignee && activeRole.assignee.resource
-                    //And not already in an accounted role
-                    && activePeopleProjectsResources.indexOf(activeRole.assignee.resource) == -1){
-                  //Just add the project to the activePeopleProjects list
-                  activePeopleProjects[activeRole.assignee.resource].push(activeProjects[i]);
+                var roleIsActive = $scope.isRoleActiveInNextSixMonths(activeRole, mm-1, yyyy);
+                if(roleIsActive){
+	                //If there is an assignee log it in the active hours
+	                if(activeRole.assignee && activeRole.assignee.resource && activeRole.rate){
+	                  var hoursPerMonth = 0;
+	                  var rate = activeRole.rate;
+	                  if(rate.fullyUtilized){
+	                    hoursPerMonth = 180;
+	                  }
+	                  else if(rate.type == 'hourly'){
+	                    hoursPerMonth = rate.hours;
+	                  }
+	                  else if(rate.type == 'weekly'){
+	                  // Weekly rate is currently hours per week. There are 5 working days per week
+	                      // and 22.5 per month.
+	                    var hoursPerMonthNotRounded = parseFloat(rate.hours * 22.5 / 5);
+	                    hoursPerMonth = Math.round(hoursPerMonthNotRounded * 100) / 100; // round to 2 decimal places
+	                  }
+	
+	
+	                  //If hours are logged from another project increment it
+	                  if(activeHours.hasOwnProperty(activeRole.assignee.resource)){
+	                    activeHours[activeRole.assignee.resource] += hoursPerMonth;
+	                  }
+	                  else{
+	                    activeHours[activeRole.assignee.resource] = hoursPerMonth;
+	                  }
+	                }
+	
+	                if(activeRole.assignee && activeRole.assignee.resource
+	                    && !activePeopleProjects.hasOwnProperty(activeRole.assignee.resource)){
+	                  //Push the assignnee onto the active list
+	                  activePeople.push(Resources.get(activeRole.assignee.resource));
+	
+	                  //Create a project list
+	                  activePeopleProjects[activeRole.assignee.resource] = [activeProjects[i]];
+	                  //Accout for person already in role in this project
+	                  activePeopleProjectsResources.push(activeRole.assignee.resource);
+	                }
+	                else if(activeRole.assignee && activeRole.assignee.resource
+	                    //And not already in an accounted role
+	                    && activePeopleProjectsResources.indexOf(activeRole.assignee.resource) == -1){
+	                  //Just add the project to the activePeopleProjects list
+	                  activePeopleProjects[activeRole.assignee.resource].push(activeProjects[i]);
+	                }
                 }
               }
             }
@@ -238,6 +256,65 @@ angular.module('Mastermind.controllers.people')
         var returnValue =  projectStarted && !projectEnded;
         return returnValue;
       };
+      
+      /**
+       * Calculates whether a role exists within a particular month.
+       *
+       * @param project
+       * @param month
+       * @param year
+       */
+      $scope.isRoleActiveInNextSixMonths = function (role, month, year) {
+        var startDay = new Date(year, month, 1),
+            monthSixMonthsFromStart = (month + 6) % 12,
+            yearSixMonthsFromStart = (month + 6) > 11 ? (year + 1) : year,
+            endDay = new Date(yearSixMonthsFromStart, monthSixMonthsFromStart, 0);
+
+        // If the project start day is before the last day of this month
+        // and its end date is after the first day of this month.
+        var roleStarted =   new Date(role.startDate) <= endDay;
+        var roleEnded = role.endDate &&  new Date(role.endDate) <= startDay;
+        var returnValue =  roleStarted && !roleEnded;
+        return returnValue;
+      };
+      
+      /**
+       * Calculates whether a role is active within a particular month.
+       *
+       * @param project
+       * @param month
+       * @param year
+       */
+      $scope.isPersonActiveInMonth = function (project, person, month, year) {
+    	  var projectIsActive = $scope.inMonth(project, month, year);
+    	  if(!projectIsActive) return false;
+    	  
+    	  //Look through all the roles
+    	  var roles = project.roles;
+    	  var personRef = person.about?person.about:person.resource;
+    	  var ret = false;
+    	  if(personRef){
+	    	  for(var i = 0; i < roles.length; i++){
+	    		  var role = roles[i];
+	    		  //Check if person is assigned to role
+	    		  if(role.assignee && personRef == role.assignee.resource){
+	    			var nextMonth = month === 11 ? 0 : (month + 1),
+	    			       nextYear = month === 11 ? (year + 1) : year,
+	    			       startDay = new Date(year, month, 1),
+	    			       endDay = new Date(nextYear, nextMonth, 0);
+
+			        // If the role start day is before the last day of this month
+			        // and its end date is after the first day of this month.
+			        var roleStarted =   new Date(role.startDate) <= endDay;
+			        var roleEnded = role.endDate &&  new Date(role.endDate) <= startDay;
+			        ret =  roleStarted && !roleEnded;
+			        if(ret) break;
+	    		  }
+	    	  }
+    	  }
+    	  
+    	  return ret;
+      };
 
       $scope.toggleTableView = function() {
         if ($scope.showGraphView) {
@@ -257,7 +334,7 @@ angular.module('Mastermind.controllers.people')
        * Move the starting date back 5 months
        */
       $scope.ganttPrev = function() {
-        $scope.startDate.setMonth($scope.startDate.getMonth() - 1);
+        $scope.startDate.setMonth($scope.startDate.getMonth() - 5);
         $scope.buildTableView();
       };
 
@@ -273,7 +350,7 @@ angular.module('Mastermind.controllers.people')
        * Move the starting date forward 5 months
        */
       $scope.ganttNext = function() {
-        $scope.startDate.setMonth($scope.startDate.getMonth() + 1);
+        $scope.startDate.setMonth($scope.startDate.getMonth() + 5);
         $scope.buildTableView();
       };
 
