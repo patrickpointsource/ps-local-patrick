556d69a9cca54a3e028ce024daf10d10e5590d4b
Kevin Bauer <kevin.bauer@pointsource.com>
2014-01-19 16:50:00 +0000

    15045 As an employee I want a quick view on the home page to enter hours for projects I am working on

--- PSMasterMind/app/scripts/controllers/main.js
+++ PSMasterMind/app/scripts/controllers/main.js
@@ -233,6 +233,24 @@ angular.module('Mastermind')
         var returnValue =  projectStarted && !projectEnded;
         return returnValue;
       };
+      
+      
+      $scope.newHoursRecord = {};
+      
+      /**
+ 	  * Add a new Hours Record to the server
+ 	  */
+ 	  $scope.addHours = function(){
+ 		 //Set the person context
+ 		 $scope.newHoursRecord.person = {resource:$scope.me.about};
+ 		  
+ 		 Resources.create('hours', $scope.newHoursRecord).then(function(){ 
+ 			$scope.newHoursRecord = {};
+ 			
+ 			//Navigate over to the users profile
+ 			window.location="#"+$scope.me.about;
+ 		 });
+ 	  }
     }]);
 
 
