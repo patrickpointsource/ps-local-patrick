af441598c90f11f65c07cb787fbe9276b7feb6bf
Kevin Bauer <kevin.bauer@pointsource.com>
2014-01-20 15:18:00 +0000

    15348 Login page flashes after login before logging in

--- PSMasterMind/app/scripts/login.js
+++ PSMasterMind/app/scripts/login.js
@@ -41,18 +41,8 @@ var helper = (function () {
         //Save the access token
         localStorage["access_token"] = authResult['access_token'];
 
-        if (!existingToken) {
-          //Reload the App
-          window.location.reload();
-        }
-
-        else {
-//          // After we load the Google+ API, render the profile data from
-//          // Google+.
-//          gapi.client.load('plus', 'v1', this.renderProfile);
-
-        	window.location = window.clientBaseURL+"index.html";
-        }
+        window.location = window.clientBaseURL+"index.html";
+      
 
       } else if (authResult['error']) {
         // There was an error, which means the user is not signed in.
@@ -62,31 +52,13 @@ var helper = (function () {
 
         //Delete the auth token
 
-        $('#authResult').append('Logged out');
-        $('#authOps').hide('slow');
-        $('#gConnect').show();
+        //$('#authResult').append('Logged out');
+        //$('#authOps').hide('slow');
+        //$('#gConnect').show();
       }
       //console.log('authResult', authResult);
     },
-    /**
-     * Retrieves and renders the authenticated user's Google+ profile.
-     */
-    renderProfile: function () {
-      var request = gapi.client.plus.people.get({
-        'userId': 'me'
-      });
-      request.execute(function (profile) {
-        $('#profile-photo').empty();
-        if (profile.error) {
-          $('#profile-photo').append(profile.error);
-          return;
-        }
-        $('#profile-photo').append(
-          $('<img src=\"' + profile.image.url + '\">'));
-      });
-      $('#authOps').show('slow');
-      $('#gConnect').hide();
-    },
+    
 
     disconnectUser: function (access_token) {
       if (access_token) {
@@ -124,39 +96,6 @@ var helper = (function () {
       }else{
       	window.location = window.clientBaseURL+"login.html";
       }
-    },
-    /**
-     * Calls the server endpoint to get the list of people visible to this
-     * app.
-     */
-    people: function () {
-      $.ajax({
-        type: 'GET',
-        url: window.location.href + 'people',
-        contentType: 'application/octet-stream; charset=utf-8',
-        success: function (result) {
-          helper.appendCircled(result);
-        },
-        processData: false
-      });
-    },
-    /**
-     * Displays visible People retrieved from server.
-     *
-     * @param {Object}
-     *            people A list of Google+ Person resources.
-     */
-    appendCircled: function (people) {
-      $('#visiblePeople').empty();
-
-      $('#visiblePeople').append(
-        'Number of people visible to this app: '
-          + people.totalItems + '<br/>');
-      for (var personIndex in people.items) {
-        person = people.items[personIndex];
-        $('#visiblePeople').append(
-          '<img src="' + person.image.url + '">');
-      }
-    },
+    }
   };
 })();
