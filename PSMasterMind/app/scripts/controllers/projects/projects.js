6b0a6b504c5dc48f0578d8c10da1aff372d48e59
Steven Behun <steven.behun@pointsource.com>
2014-01-27 17:26:00 +0000

    16138 You should not be able to delete a project from project list page

--- PSMasterMind/app/scripts/controllers/projects/projects.js
+++ PSMasterMind/app/scripts/controllers/projects/projects.js
@@ -7,44 +7,45 @@ angular.module('Mastermind.controllers.projects')
   .controller('ProjectsCtrl', [ '$scope', '$state', '$filter', 'ngTableParams', 'Resources',
     function ($scope, $state, $filter, TableParams, Resources) {
 
-	  //Default to no projects
-	  $scope.projects = [];
-	  
-	  $scope.handleProjectFilterChanged = function(){
-    	var filter = $scope.projectFilter;
-    	//Filter just the active projects
-    	if(filter == 'active'){
-    		 //Get todays date formatted as yyyy-MM-dd
-    	      var today = new Date();
-    	      var dd = today.getDate();
-    	      var mm = today.getMonth()+1; //January is 0!
-    	      var yyyy = today.getFullYear();
-    	      if(dd<10){dd='0'+dd} if(mm<10){mm='0'+mm} today = yyyy+'-'+mm+'-'+dd;
-    	      
-    	      var apQuery = {startDate:{$lte:today},$or:[{endDate:{$exists:false}},{endDate:{$gt:today}}]};
-    	      var apFields = {resource:1,name:1,"roles.assignee":1};
-    	      
-    	      Resources.query('projects', apQuery, apFields, function(result){
-    	    	  $scope.projects = result.data;
-    	    	  //Reload the Table
-    	    	  $scope.tableParams.reload();
-    	      });
-    	}
-    	else{
-		  //Default to all
-		  $scope.projectFilter = 'all';
-      	  
-		  Resources.query('projects', {}, {resource:1,name:1}, function(result){
-      		  $scope.projects = result.data;
-      		  //Reload the table with data
-      		  $scope.tableParams.reload();
-    	   });
-      	}
+    //Default to no projects
+    $scope.projects = [];
+
+    $scope.handleProjectFilterChanged = function(){
+      var filter = $scope.projectFilter;
+      //Filter just the active projects
+      if(filter == 'active'){
+        //Get todays date formatted as yyyy-MM-dd
+        var today = new Date();
+        var dd = today.getDate();
+        var mm = today.getMonth()+1; //January is 0!
+        var yyyy = today.getFullYear();
+        if(dd<10){dd='0'+dd} if(mm<10){mm='0'+mm} today = yyyy+'-'+mm+'-'+dd;
+
+        var apQuery = {startDate:{$lte:today},$or:[{endDate:{$exists:false}},{endDate:{$gt:today}}]};
+        var apFields = {resource:1,name:1,"roles.assignee":1,customerName:1};
+
+        Resources.query('projects', apQuery, apFields, function(result){
+          $scope.projects = result.data;
+          console.log($scope.projects);
+          //Reload the Table
+          $scope.tableParams.reload();
+        });
+      }
+      else{
+        //Default to all
+        $scope.projectFilter = 'all';
+
+        Resources.query('projects', {}, {resource:1,name:1,customerName:1}, function(result){
+          $scope.projects = result.data;
+          //Reload the table with data
+          $scope.tableParams.reload();
+         });
+        }
       };
-	  
+
       $scope.projectFilter = $state.params.filter?$state.params.filter:'all';
       $scope.handleProjectFilterChanged();
-      
+
       /**
        * Navigate to creating a project.
        */
@@ -52,12 +53,6 @@ angular.module('Mastermind.controllers.projects')
         $state.go('projects.new');
       };
 
-      $scope.deleteProject = function (project) {
-        Resources.remove(project).then(function () {
-        	$scope.handleProjectFilterChanged();
-        });
-      };
-      
       // Table Parameters
       var params = {
         page: 1,            // show first page
@@ -71,16 +66,12 @@ angular.module('Mastermind.controllers.projects')
         total: $scope.projects.length, // length of data
         getData: function ($defer, params) {
           var start = (params.page() - 1) * params.count(),
-            end = params.page() * params.count(),
+              end = params.page() * params.count(),
 
           // use build-in angular filter
-            orderedData = params.sorting() ?
-              $filter('orderBy')($scope.projects, params.orderBy()) :
-            	  $scope.projects,
-
+          orderedData = params.sorting() ? $filter('orderBy')($scope.projects, params.orderBy()) : $scope.projects,
               ret = orderedData.slice(start, end);
-             
-              
+
           $defer.resolve(ret);
         }
       });
