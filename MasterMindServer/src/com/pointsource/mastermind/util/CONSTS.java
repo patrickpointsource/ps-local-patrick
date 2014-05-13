d54ead73f92087d2419a2ad3450f46901171f9b9
Aditya Desai <aditya.desai@pointsource.com>
2014-05-13 13:26:00 +0000

    Changes to Consts to club prod/stage/local references together

--- MasterMindServer/src/com/pointsource/mastermind/util/CONSTS.java
+++ MasterMindServer/src/com/pointsource/mastermind/util/CONSTS.java
@@ -166,20 +166,23 @@ public interface CONSTS {
 	public String DB_HOSTNAME_DEFAULT = "localhost";
 	public int DB_PORT_DEFAULT  = 27017;
 	
-	//Local And Staging
-		public String DB_NAME_DEFAULT  = "mm_db_stage";
 	//Local Host
 		public String PUBLIC_BASE_URL = "http://localhost:8080/MasterMindStaging/";
-		//public String WEB_SITE_URL = "http://mastermind.pointsource.us:9000";
-		//public String WEB_SITE_URL = "http://localhost:9000";
 		public String WEB_SITE_URL = "http://0.0.0.0:9000";
+		//public String WEB_SITE_URL = "http://localhost:9000";
+		public String DB_NAME_DEFAULT  = "mm_db_stage";
 	//Staging Only
-		//public String PUBLIC_BASE_URL = "https://dmz.mastermind.pointsource.us/MasterMindStaging/";
+//		public String PUBLIC_BASE_URL = "https://dmz.mastermind.pointsource.us/MasterMindStaging/";
+//		public String DB_NAME_DEFAULT  = "mm_db_stage";
+//		public String WEB_SITE_URL = "https://mastermind.pointsource.us";
+	//Demo Only
+//		public String PUBLIC_BASE_URL = "https://dmz.mastermind.pointsource.us/MasterMindDemo/";
+//		public String DB_NAME_DEFAULT  = "mm_db_demo";
+//		public String WEB_SITE_URL = "https://mastermind.pointsource.us";
 	//Production Only
-		//public String DB_NAME_DEFAULT  = "mm_db_prod";
-		//public String PUBLIC_BASE_URL = "https://dmz.mastermind.pointsource.us/MasterMindServer/";
-	//Production and Staging
-		//public String WEB_SITE_URL = "https://mastermind.pointsource.us";
+//		public String DB_NAME_DEFAULT  = "mm_db_prod";
+//		public String PUBLIC_BASE_URL = "https://dmz.mastermind.pointsource.us/MasterMindServer/";
+//		public String WEB_SITE_URL = "https://mastermind.pointsource.us";
 		
 	public String COLLECTION_TITLE_HOURS = "Hours";
 	public String COLLECTION_TITLE_LINKS = "Links";
