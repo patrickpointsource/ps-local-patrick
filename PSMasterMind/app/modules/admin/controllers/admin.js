746bfe8ebf7e61fa720f6b36cca7909d22793a6a
Ross Inman <ross.inman@pointsource.com>
2014-05-14 21:08:00 +0000

    Added admin groups view and controller. Needs to be put in it's own controller and some issues with glyphicon not appearing. Will push those fixes tomorrow.

--- PSMasterMind/app/modules/admin/controllers/admin.js
+++ PSMasterMind/app/modules/admin/controllers/admin.js
@@ -2,10 +2,11 @@
 
 /*
  * Controller for navigating through areas of Mastermind like its dashboard,
- * projects, people, and roles.
+ * projects, people, groups and roles.
  */
-angular.module('Mastermind').controller('AdminCtrl', ['$scope', '$state','$filter', '$q', 'Resources','ngTableParams',
-  function ($scope, $state, $filter, $q, Resources, TableParams) {
+angular.module('Mastermind').controller('AdminCtrl', ['$scope', '$state','$filter', '$q', 
+                                                      'Resources','ngTableParams','ngTableParams',
+  function ($scope, $state, $filter, $q, Resources, TableParams, ngTableParams) {
 	
 	$scope.navType = 'pills';
 	  
@@ -145,4 +146,70 @@ angular.module('Mastermind').controller('AdminCtrl', ['$scope', '$state','$filte
         });
       });
     };
-  }]);
+    
+    
+    //////////////////////////////////////////////////////////////
+    // Groups //
+    //////////////////////////////////////////////////////////////   
+    
+    /**
+     * Get the list of people
+     * if person belongs in group, inject them into the group object
+     * 
+     * Currently Hard coded as {'Executives', 'Management', 'Project Management', 'Sales'}
+     */
+    
+    Resources.refresh('people').then(function(result){
+        $scope.members = result.members;
+        $scope.memberCount = result.count;
+        $scope.groups = [{label: 'Executives', members: []}, 
+    	                 {label: 'Management', members: []}, 
+    	                 {label: 'Project Management', members: []}, 
+    	                 {label: 'Sales', members: []}];
+        //Map the labels of the groups so I can match faster
+        var groupsMap = {};
+        $scope.groups.forEach(function(entry) {
+        	groupsMap[entry.label] = entry;
+        });
+        
+        // Count the number of entries into groups
+        var rowCount = 0;
+
+        for ( var i = 0 ; i < $scope.memberCount ; i++ ) {
+            var member = $scope.members[ i ];
+            if(member.groups) {
+            	for (var group in member.groups) {
+            		// Match the group label
+            		var groupLabel = member.groups[ group ];
+            		// push the member into the group
+            		groupsMap[ groupLabel ].members.push(member);
+            		rowCount++;
+            	}
+            }
+        }
+        $scope.tableParams = new ngTableParams({
+            page: 1,            // show first page
+            count: 10          // count per page
+        }, {
+            groupBy: 'groups',
+            total: rowCount,
+            getData: function($defer, params) {
+              var orderedData = params.sorting() ?
+                      $filter('orderBy')($scope.groups, $scope.tableParams.orderBy()) :
+                    	  $scope.groups;
+
+              $defer.resolve(orderedData.slice((params.page() - 1) * params.count(), 
+            		  		 params.page() * params.count()));
+            }
+        });
+    });
+
+}]);
+
+// Minimizes the Management group - added in case they want all collapsed at start
+angular.module('Mastermind').controller('groupCtrl', function($scope) {
+	  console.log($scope.groups);
+	  if ($scope.group.label != 'Management')
+	    $scope.groups.$hideRows = true;
+	  console.log($scope.groups);
+	});
