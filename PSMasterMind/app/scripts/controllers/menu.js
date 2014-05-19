b619d6813fc4df51b9145b4a8c5b9e57c000661d
Tara Maxwell <tara.maxwell@pointsource.com>
2014-05-19 13:33:00 +0000

    Updated styles for main menu

--- PSMasterMind/app/scripts/controllers/menu.js
+++ PSMasterMind/app/scripts/controllers/menu.js
@@ -7,18 +7,18 @@
 angular.module('Mastermind').controller('MenuCtrl', ['$scope', '$state','$filter', '$q',
   function ($scope, $state, $filter, $q) {
 	//$scope.projectManagementAccess = false;
-	
+
 	$scope.menuItems = [ {
 		text: "Dashboard",
 		value: "home",
 		handler: "showHome",
-		iconCss: "fa-home",
+		iconCss: "icon-dashboard",
 		isRender: "true"
 	}, {
 		text: "Projects",
 		value: "projects",
 		handler: "showProjects",
-		iconCss: "fa-briefcase",
+		iconCss: "icon-projects",
 		isRender: "true",
 		subItems: [ {
 			text: "All Statuses",
@@ -63,12 +63,12 @@ angular.module('Mastermind').controller('MenuCtrl', ['$scope', '$state','$filter
 			value: "allstate",
 			handler: "handleSubitem"
 		}*/]
-		
+
 	}, {
 		text: "People",
 		value: "people",
 		handler: "showPeople",
-		iconCss: "fa-users",
+		iconCss: "icon-people",
 		isRender: "true",
 		subItems: [ {
 			text: "All People",
@@ -108,22 +108,22 @@ angular.module('Mastermind').controller('MenuCtrl', ['$scope', '$state','$filter
 		text: "Staffing",
 		value: "staffing",
 		handler: "showStaffing",
-		iconCss: "fa-tasks",
+		iconCss: "icon-staffing",
 		isRender: $scope.projectManagementAccess
 	}, {
 		text: "Administration",
 		value: "admin",
 		handler: "showAdmin",
-		iconCss: "fa-gear",
+		iconCss: "icon-admin",
 		isRender: $scope.adminAccess
 	}];
-	
+
 	$scope.isSubitemSelected = function(subItem, item) {
 		var result = false;
 		var val = subItem.value;
 		var f = $scope.getActiveAreaFilter();
 		var tmp = f ? f.split(','): [];
-		
+
 		for (var i = 0; i < tmp.length; i ++) {
 			if (tmp[i].trim().toLowerCase() == val) {
 				result = true;
@@ -133,68 +133,68 @@ angular.module('Mastermind').controller('MenuCtrl', ['$scope', '$state','$filter
 		/*
 		if (!result && !subItem.subheader) {
 			var start = _.indexOf(item.subItems, subItem)
-			
+
 			var i = start;
-			
+
 			for (; i >= 0 && !item.subItems[i].subheader; i --){}
-			
+
 			result = item.subItems[i].active;
 		}
 		*/
 		subItem.active = result;
-		
+
 		return result;
 	}
-	
+
 	$scope.handleClick = function(handler, menuItem, subItem, subIndex) {
 		var e = window.event;
-		
+
 		if (this[handler]) {
 			var li = $(e.target).closest('li');
-			
+
 			li.find('ul.subnavbar').removeClass('hidden');
-			
+
 			this[handler]();
 		} else if (handler == "handleSubitem" && subItem){
-			
-			
+
+
 			if (!subItem.subheader){
 				for (var i = subIndex - 1; menuItem.subItems[i]; i --)
 					if (menuItem.subItems[i].subheader) {
 						menuItem.subItems[i].active = false;
 						break;
 					}
-						
+
 			} /*else {
 				for (var i = subIndex + 1; menuItem.subItems[i] && !menuItem.subItems[i].subheader; i ++)
 					menuItem.subItems[i].active =  !subItem.active;
-						
+
 			}*/
-			
+
 			var selected = [];
 
 			subItem.active = !subItem.active;
-			
+
 			for (var i = 0; i < menuItem.subItems.length; i ++)
 				if (menuItem.subItems[i].active)
 					selected.push(menuItem.subItems[i].value)
-			
+
 			if (_.find(selected, function(v) {return v == "all"}))
 				this[menuItem.handler]("all")
 			else
 				this[menuItem.handler](selected.join(','));
-			
-			
+
+
 		}
-		
-		
-		
+
+
+
 		if (e) {
 			e.stopPropagation()
 		}
 	}
-	
+
 	if (!$scope.additionalClass)
 		$scope.additionalClass = 'navbar-inverse visible-xs visible-sm';
-  
+
   }]);
