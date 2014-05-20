c7f98b4ffe5acc496698ae3f3b29b0352d9ee317
Tara Maxwell <tara.maxwell@pointsource.com>
2014-05-20 21:54:00 +0000

    Updated styles for header; additional styling for left nav; and "cleaning"

--- PSMasterMind/app/scripts/controllers/areas.js
+++ PSMasterMind/app/scripts/controllers/areas.js
@@ -4,22 +4,24 @@
  * Controller for navigating through areas of Mastermind like its dashboard,
  * projects, people, and roles.
  */
-angular.module('Mastermind').controller('AreasCtrl', ['$scope', '$state', '$rootScope','Resources', 'ProjectsService',
-  function ($scope, $state,  $rootScope, Resources, ProjectsService) {
+angular.module('Mastermind').controller('AreasCtrl', ['$scope', '$state', '$rootScope', 'Resources', 'ProjectsService',
+  function($scope, $state, $rootScope, Resources, ProjectsService) {
 
-	// make these vars accessible in scope methods - especially "showHome"
-	var apQuery;
-	var apFields;
+    // make these vars accessible in scope methods - especially "showHome"
+    var apQuery;
+    var apFields;
 
-    $scope.widgets = [
-      {'name' : 'Hours', 'templateLocation' : 'modules/widgets/hours/hours.html', 'available' : ['you', 'me', 'them']}
-    ]
+    $scope.widgets = [{
+      'name': 'Hours',
+      'templateLocation': 'modules/widgets/hours/hours.html',
+      'available': ['you', 'me', 'them']
+    }];
 
     // Default dashboard view overwritten below if Exec or Management
     $scope.dashboardScreen = 'views/dashboards/baseDashboard.html';
 
     //Load my profile for group and role checking
-    Resources.refresh('people/me').then(function(me){
+    Resources.refresh('people/me').then(function(me) {
       $scope.me = me;
 
       /**
@@ -33,56 +35,56 @@ angular.module('Mastermind').controller('AreasCtrl', ['$scope', '$state', '$root
        * Update Role Types (adminAccess)
        * Can Assign Users to Groups (adminAccess)
        */
-      if(me.groups && me.groups.indexOf('Executives') !== -1){
+      if (me.groups && me.groups.indexOf('Executives') !== -1) {
         $scope.financeAccess = true;
         $scope.adminAccess = true;
         $scope.projectManagementAccess = true;
         $scope.dashboardScreen = 'views/dashboards/execDashboard.html';
       }
 
-	  /**
-	   * Members of the 'Management' group...
-	   *
-	   * Can edit any project (projectManagementAccess)
-	   * Can view all financial info (financeAccess)
-	   * Can make project assignments (projectManagementAccess)
-	   * View Staffing Deficits (projectManagementAccess)
-	   * Update Role Types (adminAccess)
-	   * Can Assign Users to Groups (adminAccess)
-	   */
-	 	if(me.groups && me.groups.indexOf('Management') !== -1){
-	      $scope.financeAccess = true;
-	      $scope.adminAccess = true;
-	      $scope.projectManagementAccess = true;
+      /**
+       * Members of the 'Management' group...
+       *
+       * Can edit any project (projectManagementAccess)
+       * Can view all financial info (financeAccess)
+       * Can make project assignments (projectManagementAccess)
+       * View Staffing Deficits (projectManagementAccess)
+       * Update Role Types (adminAccess)
+       * Can Assign Users to Groups (adminAccess)
+       */
+      if (me.groups && me.groups.indexOf('Management') !== -1) {
+        $scope.financeAccess = true;
+        $scope.adminAccess = true;
+        $scope.projectManagementAccess = true;
         $scope.dashboardScreen = 'views/dashboards/managerDashboard.html';
-	    }
+      }
 
-		/**
-		 * Members of the 'Project Management' group...
-		 *
-		 * Can edit any project (projectManagementAccess)
-		 * Can make project assignments (projectManagementAccess)
-		 * View Staffing Deficits (projectManagementAccess)
-		 */
-		if(me.groups && me.groups.indexOf('Project Management') !== -1){
-		    $scope.projectManagementAccess = true;
+      /**
+       * Members of the 'Project Management' group...
+       *
+       * Can edit any project (projectManagementAccess)
+       * Can make project assignments (projectManagementAccess)
+       * View Staffing Deficits (projectManagementAccess)
+       */
+      if (me.groups && me.groups.indexOf('Project Management') !== -1) {
+        $scope.projectManagementAccess = true;
         $scope.dashboardScreen = 'views/dashboards/managerDashboard.html';
-		}
+      }
 
-		/**
-		 * Members of the 'Sales' group...
-		 *
-		 * Is in the Sales Sponsor List (queried from People collection)
-		 * Can view all financial info (financeAccess)
-		 */
-		if(me.groups && me.groups.indexOf('Sales') !== -1){
-		    $scope.financeAccess = true;
-		}
+      /**
+       * Members of the 'Sales' group...
+       *
+       * Is in the Sales Sponsor List (queried from People collection)
+       * Can view all financial info (financeAccess)
+       */
+      if (me.groups && me.groups.indexOf('Sales') !== -1) {
+        $scope.financeAccess = true;
+      }
 
-		//console.log('Logged In');
-		$scope.authState = true;
+      //console.log('Logged In');
+      $scope.authState = true;
 
-		$scope.$emit('me:loaded')
+      $scope.$emit('me:loaded')
     });
 
     /**
@@ -101,7 +103,7 @@ angular.module('Mastermind').controller('AreasCtrl', ['$scope', '$state', '$root
       } else if ($state.includes('staffing')) {
         area = 'staffing';
       } else if ($state.includes('admin')) {
-          area = 'admin';
+        area = 'admin';
       }
 
       return area;
@@ -109,115 +111,123 @@ angular.module('Mastermind').controller('AreasCtrl', ['$scope', '$state', '$root
 
     $scope.activeArea = activeArea;
 
-    $scope.getActiveAreaFilter = function(){
-    	return $state.params ? $state.params.filter: "all"
+    $scope.getActiveAreaFilter = function() {
+      return $state.params ? $state.params.filter : "all"
     }
 
     $scope.showHideMenu = function() {
 
-    	var isToBeShown = !$('#navbar-collapse-mobile').hasClass('in');
-    	//var menuWidth = $('#navbar-collapse-mobile').width();
-    	var menuWidth = 280;
-    	var minTopHeaderContentWidth = 90;
+      var isToBeShown = !$('#navbar-collapse-mobile').hasClass('in');
+      //var menuWidth = $('#navbar-collapse-mobile').width();
+      var menuWidth = 280;
+      var minTopHeaderContentWidth = 90;
 
-    	if (isToBeShown) {
-    		if(!$('#appContent').data('origWidth'))
-    			$('#appContent').data('origWidth', $('#appContent').width())
+      if (isToBeShown) {
+        if (!$('#appContent').data('origWidth'))
+          $('#appContent').data('origWidth', $('#appContent').width())
 
-    		$('#appContent').css('width', $('#appContent').data('origWidth'));
-    		$('#appContent').parent().css('overflow', 'hidden')
+        $('#appContent').css('width', $('#appContent').data('origWidth'));
+        $('#appContent').parent().css('overflow', 'hidden')
 
-	    	$('#appContent').animate({
-	    		marginLeft: menuWidth + 'px'
-	    	});
+        $('#appContent').animate({
+          marginLeft: menuWidth + 'px'
+        });
 
-    		var paddingLeft = (menuWidth - 20);
-    		var topHeaderWidth = $(".navbar.navbar-default.navbar-fixed-top").width();
+        var paddingLeft = (menuWidth - 20);
+        var topHeaderWidth = $(".navbar.navbar-default.navbar-fixed-top").width();
 
-    		if ((topHeaderWidth - paddingLeft) < minTopHeaderContentWidth){
-    			var w = $(".navbar.navbar-default.navbar-fixed-top").width();
+        if ((topHeaderWidth - paddingLeft) < minTopHeaderContentWidth) {
+          var w = $(".navbar.navbar-default.navbar-fixed-top").width();
 
-    			$(".navbar.navbar-default.navbar-fixed-top").data('origWidth', w)
+          $(".navbar.navbar-default.navbar-fixed-top").data('origWidth', w)
 
-    			$(".navbar.navbar-default.navbar-fixed-top").width( w + minTopHeaderContentWidth - (topHeaderWidth - paddingLeft) );
-    		}
+          $(".navbar.navbar-default.navbar-fixed-top").width(w + minTopHeaderContentWidth - (topHeaderWidth - paddingLeft));
+        }
 
-    		$(".navbar.navbar-default.navbar-fixed-top").animate({
-	    		paddingLeft:  paddingLeft + 'px'
-	    	});
-    	} else {
-    		$('#appContent').animate({
-	    		marginLeft: '0px'
-	    	}, function() {
-	    		$('#appContent').css('width', 'auto');
-	    		$('#appContent').parent().css('overflow', 'auto')
-	    	});
+        $(".navbar.navbar-default.navbar-fixed-top").animate({
+          paddingLeft: paddingLeft + 'px'
+        });
+      } else {
+        $('#appContent').animate({
+          marginLeft: '0px'
+        }, function() {
+          $('#appContent').css('width', 'auto');
+          $('#appContent').parent().css('overflow', 'auto')
+        });
 
-    		$(".navbar.navbar-default.navbar-fixed-top").animate({
-	    		paddingLeft: '0px'
-	    	}, function() {
-	    		if ($(".navbar.navbar-default.navbar-fixed-top").data('origWidth'))
-	    			$(".navbar.navbar-default.navbar-fixed-top").width($(".navbar.navbar-default.navbar-fixed-top").data('origWidth'))
-	    	});
-    	}
+        $(".navbar.navbar-default.navbar-fixed-top").animate({
+          paddingLeft: '0px'
+        }, function() {
+          if ($(".navbar.navbar-default.navbar-fixed-top").data('origWidth'))
+            $(".navbar.navbar-default.navbar-fixed-top").width($(".navbar.navbar-default.navbar-fixed-top").data('origWidth'))
+        });
+      }
 
-    	$('#navbar-collapse-mobile').collapse('toggle');
+      $('#navbar-collapse-mobile').collapse('toggle');
     }
 
     $scope.showMainMenu = function(e) {
-    	e = e ? e: window.event;
-    	e.preventDefault();
-    	e.stopPropagation();
+      e = e ? e : window.event;
+      e.preventDefault();
+      e.stopPropagation();
 
-    	var subnavbar = $(e.target).closest('.subnavbar');
+      var subnavbar = $(e.target).closest('.subnavbar');
 
-    	subnavbar.addClass('hidden');
-    	return false;
+      subnavbar.addClass('hidden');
+      return false;
     }
     /*
      * Navigate to the dashboard.
      */
-    $scope.showHome = function () {
-    	ProjectsService.getMyCurrentProjects($scope.me).then(function(result){
-	      	  $scope.myActiveProjects =  result.data;
-	  	      if(result.data.length>0){
-	  	          $scope.hasActiveProjects = true;
-	  	      }
-	     });
-    	 $state.go('home');
+    $scope.showHome = function() {
+      ProjectsService.getMyCurrentProjects($scope.me).then(function(result) {
+        $scope.myActiveProjects = result.data;
+        if (result.data.length > 0) {
+          $scope.hasActiveProjects = true;
+        }
+      });
+      $state.go('home');
     };
 
     /*
      * Navigate to the projects index.
      */
-    $scope.showProjects = function (filter) {
-    	if (!filter)
-    		$state.go('projects.index', {filter:'all'});
-    	else
-    		$state.go('projects.index', {filter:filter});
+    $scope.showProjects = function(filter) {
+      if (!filter)
+        $state.go('projects.index', {
+          filter: 'all'
+        });
+      else
+        $state.go('projects.index', {
+          filter: filter
+        });
     };
 
     /*
      * Navigate to the projects index.
      */
-    $scope.showPeople = function (filter) {
-    	if (!filter)
-    		$state.go('people.index', {filter:'all'});
-    	else
-    		$state.go('people.index', {filter:filter});
+    $scope.showPeople = function(filter) {
+      if (!filter)
+        $state.go('people.index', {
+          filter: 'all'
+        });
+      else
+        $state.go('people.index', {
+          filter: filter
+        });
     };
 
     /*
      * Navigate to the staffing index.
      */
-    $scope.showStaffing = function () {
+    $scope.showStaffing = function() {
       $state.go('staffing');
     };
 
     /*
      * Navigate to the projects index.
      */
-    $scope.showAdmin = function () {
+    $scope.showAdmin = function() {
       $state.go('admin');
     };
 
@@ -226,51 +236,47 @@ angular.module('Mastermind').controller('AreasCtrl', ['$scope', '$state', '$root
      *
      * You must pass the startDate,endDate,type and committed
      */
-    $scope.projectState = function(project){
-    	var state = ProjectsService.getProjectState(project);
-    	return state;
+    $scope.projectState = function(project) {
+      var state = ProjectsService.getProjectState(project);
+      return state;
     };
 
     /**
      * Get the icon classes associated with a project state
      */
-    $scope.projectStateIcon = function(project){
-    	var state = $scope.projectState(project);
-    	var ret = '';
-    	if(state == 'Investment'){
-    		ret = 'fa fa-flask';
-    	}
-    	else if(state == 'Pipeline'){
-    		ret = 'fa fa-angle-double-left';
-    	}
-    	else if(state == 'Backlog'){
-    		ret = 'fa fa-angle-left';
-    	}
-    	else if(state == 'Active'){
-    		ret = 'fa fa-rocket';
-    	}
-    	else if(state == 'Done'){
-    		ret = 'fa fa-times-circle-o';
-    	}
-    	else if(state == 'Deal Lost'){
-    		ret = 'fa fa-minus-circle';
-    	}
+    $scope.projectStateIcon = function(project) {
+      var state = $scope.projectState(project);
+      var ret = '';
+      if (state === 'Investment') {
+        ret = 'fa fa-flask';
+      } else if (state === 'Pipeline') {
+        ret = 'fa fa-angle-double-left';
+      } else if (state === 'Backlog') {
+        ret = 'fa fa-angle-left';
+      } else if (state === 'Active') {
+        ret = 'fa fa-rocket';
+      } else if (state === 'Done') {
+        ret = 'fa fa-times-circle-o';
+      } else if (state === 'Deal Lost') {
+        ret = 'fa fa-minus-circle';
+      }
 
 
 
-    	return ret;
-    }
+      return ret;
+    };
 
     $scope.getModal = function() {
-    	return $rootScope.modalDialog;
-    }
-
-  }]).directive('backImg', function(){
-    return function(scope, element, attrs){
-      var url = attrs.backImg;
-      element.css({
-        'background-image': 'url(' + url +')',
-        'background-size' : 'cover'
-      });
+      return $rootScope.modalDialog;
     };
-  });
+
+  }
+]).directive('backImg', function() {
+  return function(scope, element, attrs) {
+    var url = attrs.backImg;
+    element.css({
+      'background-image': 'url(' + url + ')',
+      'background-size': 'cover'
+    });
+  };
+});
