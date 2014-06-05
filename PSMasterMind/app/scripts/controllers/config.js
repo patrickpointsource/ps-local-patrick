4cecebcf416d5d01001abf269ab0a9fd5bc0c766
Daniil Dziaruhin <daniil.dziaruhin@pointsource.com>
2014-06-05 13:26:00 +0000

    28429 Need the ability to modify the server list that JazzHub projects can live on, on the Admin tab
    Editable config properties for admin

--- PSMasterMind/app/scripts/controllers/config.js
+++ PSMasterMind/app/scripts/controllers/config.js
@@ -4,26 +4,40 @@
  * Used for services configuration properties
  */
 
-angular.module('Mastermind').controller('ConfigCtrl', ['$scope', '$filter', '$q', 'Resources', 'ngTableParams',
-   function ($scope, $filter, $q, Resources, TableParams) {
-		var params = {
-		  page: 1,            // show first page
-		  count: -1,           // hide pagination
-		  sorting: {
-		    type: 'asc'     // initial sorting
-		  }
-		};
-	
-		Resources.refresh('config/services').then(function(result) {
-			$scope.config = result;
-			$scope.configTableParams = new TableParams(params,{
-				total: $scope.config.properties.length, // length of data
-				counts: [], // hide items per page
-				getData: function ($defer, params) {
-					$defer.resolve($scope.config);
-				}
-			});
-		});
-	
+angular.module('Mastermind').controller('ConfigCtrl', ['$scope', '$filter', '$q', 'Resources',
+   function ($scope, $filter, $q, Resources) {
 
+		$scope.editIndex = null;
+		
+		$scope.edit = function(index) {
+			if($scope.editIndex == index) {
+				$scope.editIndex = null;
+			} else {
+				$scope.editIndex = index;
+			}
+		}
+		
+		$scope.save = function(index) {
+			$scope.config.resource = 'config/services';
+			Resources.update($scope.config).then(function(result) {
+				$scope.refreshConfig();
+			});
+			
+			$scope.editIndex = null;
+		}
+		
+		$scope.cancel = function() {
+			$scope.editIndex = null;
+			$scope.refreshConfig();
+		}
+		
+		$scope.config = {};
+		
+		$scope.refreshConfig = function() {
+			Resources.refresh('config/services').then(function(result) {
+				$scope.config = result;
+			});
+		}
+		
+		$scope.refreshConfig();
 }]);
