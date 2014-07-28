64ba055205887209d645856026de49e662588bb1
Denys Novalenko <denys.novalenko@pointsource.com>
2014-07-28 07:31:00 +0000

    40186 Implement CRUD services in MasterMindNode for Projects (with project members)
    Implement CRUD services in MasterMindNode for Projects (with project members)

--- MasterMindNode/server/controllers/projects.js
+++ MasterMindNode/server/controllers/projects.js
@@ -13,3 +13,141 @@ module.exports.listProjects = function(query, callback) {
         }
     });
 };
+
+module.exports.getProject = function(id, callback) {
+    dataAccess.getItem(id, function(err, body){
+        if (err) {
+            console.log(err);
+            callback(err, null);
+        } else {
+            callback(null, body);
+        }
+    });
+};
+
+module.exports.addProjectLink = function(id, obj, callback) {
+	obj.project.resource = "projects/" + id;
+    dataAccess.insertItem(obj._id, obj, dataAccess.Links, function(err, body){
+        if (err) {
+            console.log(err);
+            callback('error insert skill', null);
+        } else {
+            callback(null, body);
+        }
+    });
+};
+
+
+module.exports.listLinks = function(id, callback) {
+	
+	//TODO filter by project id
+	
+    dataAccess.listLinks(query, function(err, body){
+        if (err) {
+            console.log(err);
+            callback('error loading links by project', null);
+        } else {
+            //console.log(body);
+            callback(null, body);
+        }
+    });
+
+};
+
+module.exports.listAssignments = function(id, callback) {
+	
+	//TODO filter by project id
+	
+    dataAccess.listAssignments(query, function(err, body){
+        if (err) {
+            console.log(err);
+            callback('error loading assignments by project', null);
+        } else {
+            //console.log(body);
+            callback(null, body);
+        }
+    });
+
+};
+
+module.exports.listRoles = function(callback) {
+
+	//TODO filter by project id
+
+    dataAccess.listRoles(null, function(err, body){
+        if (err) {
+            console.log(err);
+            callback('error loading roles', null);
+        } else {
+            //console.log(body);
+            callback(null, body);
+        }
+    });
+};
+
+module.exports.getRole = function(projectId, roleId, callback) {
+    dataAccess.getItem(roleId, function(err, body){
+        if (err) {
+            console.log(err);
+            callback(err, null);
+        } else {
+            callback(null, body);
+        }
+    });
+};
+
+module.exports.insertAssignment = function(projectId, obj, callback) {
+    dataAccess.insertItem(obj._id, obj, dataAccess.ASSIGNMENTS_KEY, function(err, body){
+        if (err) {
+            console.log(err);
+            callback('error insert assignment into project', null);
+        } else {
+            callback(null, body);
+        }
+    });
+};
+
+module.exports.deleteProject = function(obj, callback) {
+    dataAccess.deleteItem(obj._id, obj._rev, dataAccess.PROJECTS_KEY, function(err, body){
+        if (err) {
+            console.log(err);
+            callback(err, null);
+        } else {
+            callback(null, body);
+        }
+    });
+};
+
+module.exports.deleteProjectLink = function(projectId, linkId, obj, callback) {
+    dataAccess.deleteItem(linkId, null, dataAccess.LINKS_KEY, function(err, body){
+        if (err) {
+            console.log(err);
+            callback(err, null);
+        } else {
+            callback(null, body);
+        }
+    });
+};
+
+module.exports.insertProject = function(obj, callback) {
+    dataAccess.insertItem(obj._id, obj, dataAccess.PROJECTS_KEY, function(err, body){
+        if (err) {
+            console.log(err);
+            callback('error insert project', null);
+        } else {
+            callback(null, body);
+        }
+    });
+};
+
+module.exports.insertProjectLink = function(projectId, linkId, obj, callback) {
+	obj._id = linkId;
+    dataAccess.insertItem(obj._id, obj, dataAccess.LINKS_KEY, function(err, body){
+        if (err) {
+            console.log(err);
+            callback('error insert project', null);
+        } else {
+            callback(null, body);
+        }
+    });
+};
--- MasterMindNode/server/routes/projects.js
+++ MasterMindNode/server/routes/projects.js
@@ -19,4 +19,152 @@ router.get('/', util.isAuthenticated, function(req, res){
     });
 }); 
 
+router.get('/:id', function(req, res) {
+	var id = req.params.id;
+    projects.getProject(id, function(err, result){
+        if(err){
+            res.json(500, err);
+        } else {
+            res.json(result);
+        }            
+    });
+});
+
+router.post('/:id/links', function(req, res) {
+	var id = req.params.id;
+    projects.addProjectLink(id, req.body, function(err, result){
+        if(err){
+            res.json(500, err);
+        } else {
+            res.json(result);
+        }            
+    });
+});
+
+router.get('/:id/links', function(req, res) {
+	var id = req.params.id;
+    var query = req.query["query"] ? JSON.parse(req.query["query"]): {};
+    projects.listLinks(id, query, function(err, result){
+        if(err){
+            res.json(500, err);
+        } else {
+            res.json(result);
+        }            
+    });
+});
+
+router.get('/:id/assignments', function(req, res) {
+	var id = req.params.id;
+    var query = req.query["query"] ? JSON.parse(req.query["query"]): {};
+    projects.listAssignments(id, query, function(err, result){
+        if(err){
+            res.json(500, err);
+        } else {
+            res.json(result);
+        }            
+    });
+});
+
+router.get('/:id/roles', function(req, res) {
+	var id = req.params.id;
+    projects.listRoles(id, function(err, result){
+        if(err){
+            res.json(500, err);
+        } else {
+            res.json(result);
+        }            
+    });
+});
+
+router.get('/:id/roles', function(req, res) {
+	var id = req.params.id;
+    projects.listRoles(id, function(err, result){
+        if(err){
+            res.json(500, err);
+        } else {
+            res.json(result);
+        }            
+    });
+});
+
+router.get('/:id/roles/:roleId', function(req, res) {
+	var id = req.params.id;
+	var roleId = req.params.roleId;
+    projects.getRole(id, roleId, function(err, result){
+        if(err){
+            res.json(500, err);
+        } else {
+            res.json(result);
+        }            
+    });
+});
+
+router.put('/:id/assigments', function(req, res) {
+	var id = req.params.id;
+    projects.insertAssignment(id, req.body, function(err, result){
+        if(err){
+            res.json(500, err);
+        } else {
+            res.json(result);
+        }            
+    });
+});
+
+router.delete('/', function(req, res) {
+    projects.deleteProject(req.body, function(err, result){
+        if(err){
+            res.json(500, err);
+        } else {
+            res.json(result);
+        }            
+    });
+});
+
+router.delete('/:id/links/:linkId', function(req, res) {
+	var id = req.params.id;
+	var linkId = req.params.linkId;
+    projects.deleteProjectLink(id, linkId, function(err, result){
+        if(err){
+            res.json(500, err);
+        } else {
+            res.json(result);
+        }            
+    });
+});
+
+router.post('/', function(req, res) {
+    projects.insertProject(req.body, function(err, result){
+        if(err){
+            res.json(500, err);
+        } else {
+            res.json(result);
+        }            
+    });
+});
+
+router.put('/:id/links/:linkId', function(req, res) {
+	var id = req.params.id;
+	var linkId = req.params.linkId;
+    projects.insertProjectLink(id, linkId, req.body, function(err, result){
+        if(err){
+            res.json(500, err);
+        } else {
+            res.json(result);
+        }            
+    });
+});
+
+router.put('/:id', function(req, res) {
+	var id = req.params.id;
+	var project = req.body;
+	project._id = id;
+    projects.insertProject(project, function(err, result){
+        if(err){
+            res.json(500, err);
+        } else {
+            res.json(result);
+        }            
+    });
+});
+
 module.exports = router;
