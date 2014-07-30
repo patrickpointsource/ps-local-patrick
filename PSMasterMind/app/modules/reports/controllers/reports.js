61b6c0a09b16fb11dca83c958bffdafe5fcb90fb
Vladimir Yancharuk <vladimir.yancharuk@pointsource.com>
2014-07-30 13:47:00 +0000

    34232 Creation of the new tab for reporting on tasks by person for a custom date.

--- PSMasterMind/app/modules/reports/controllers/reports.js
+++ PSMasterMind/app/modules/reports/controllers/reports.js
@@ -6,86 +6,182 @@
 angular.module( 'Mastermind' ).controller( 'ReportsCtrl', [ '$scope', '$q', '$state', '$stateParams', '$filter', 'Resources', 'AssignmentService', 'ProjectsService', 'TasksService', 'RolesService', 'ngTableParams',
 function( $scope, $q, $state, $stateParams, $filter, Resources, AssignmentService, ProjectsService, TasksService, RolesService, TableParams ) {
 
-    $scope.activeTab = {
-        'hours': true
-    };
-    
-    $scope.reportTypes = {
-        'custom': true
-    };
-    
-    $scope.reportTerms = {
-        'week': true
-    };
-    
-    $scope.reportClick = function( item ) {
-        alert( item.name );
-    };
+	$scope.activeTab = {
+		'hours': true
+	};
 
-    $scope.tabSelected = function(tabName) {
-        var prop;
-        
-        for (prop in $scope.activeTab) {
-            $scope.activeTab[prop] = false;
-        }
-        
-        $scope.activeTab[tabName] = true;
-    };
-    
-    $scope.userRoles = {};
-    $scope.arrangedRoles = [[{abbreviation: "Select all roles", value: "all"}], [], []];
-    
-    $scope.loadRoles = function() {
-        Resources.get( 'roles' ).then( function( result ) {
-           
-            
-            var countInRow = Math.floor((result.members.length + 1) / 3);
-            var ind = 0;
-            var nestedInd = 0;
-            
-            var i = 0;
-            //Get list of roles to query members
-            for(i = 0; i < result.members.length; i++ ) {
-                var role = result.members[ i ];
-                var resource = role.resource;
-                
-                
-                ind = Math.floor(i / countInRow);
-                nestedInd = ind > 0 ? i % countInRow: (i % countInRow + 1);
-                
-                if (ind == 2 )
-                    nestedInd = i - 2 * countInRow;
-                    
-                $scope.arrangedRoles[ind][nestedInd] = role;
-                role.value = role.abbreviation.toLowerCase();
-                
-                $scope.userRoles[role.value] = false;
-            }
+	$scope.reportTypes = {
+		'custom': true
+	};
 
-        });
-    };
-    
-    $scope.selectReportTerms = function(e, term) {
-        var prop;
-        
-        for (prop in $scope.reportTerms) {
-            $scope.reportTerms[prop] = false;
-        }
-        
-        $scope.reportTerms[term] = true;
-    };
-    
-    $scope.init = function() {
-        $scope.loadRoles();
-    };
-    
-    $scope.selectReportType = function(e, report) {
-        for (var prop in $scope.reportTypes) {
-            $scope.reportTypes[prop] = false;
-        }
-        
-        $scope.reportTypes[report] = true;
-    };
-    
-    $scope.init();
-} ] ); 
+	$scope.reportTerms = {
+		'week': true
+	};
+
+	$scope.reportClick = function( item ) {
+		alert( item.name );
+	};
+
+	$scope.tabSelected = function( tabName ) {
+		var prop;
+
+		for( prop in $scope.activeTab ) {
+			$scope.activeTab[ prop ] = false;
+		}
+
+		$scope.activeTab[ tabName ] = true;
+	};
+
+	$scope.userRoles = {};
+	$scope.arrangedRoles = [ [ {
+		abbreviation: "Select all roles",
+		value: "all"
+	} ], [ ], [ ] ];
+
+	$scope.loadRoles = function( ) {
+		Resources.get( 'roles' ).then( function( result ) {
+
+			var countInRow = Math.floor( ( result.members.length + 1 ) / 3 );
+			var ind = 0;
+			var nestedInd = 0;
+
+			var i = 0;
+			//Get list of roles to query members
+			for( i = 0; i < result.members.length; i++ ) {
+				var role = result.members[ i ];
+				var resource = role.resource;
+
+				ind = Math.floor( i / countInRow );
+				nestedInd = ind > 0 ? i % countInRow : ( i % countInRow + 1 );
+
+				if( ind == 2 )
+					nestedInd = i - 2 * countInRow;
+
+				$scope.arrangedRoles[ind][ nestedInd ] = role;
+				role.value = role.abbreviation.toLowerCase( );
+
+				$scope.userRoles[ role.value ] = {
+					value: false,
+					resource: role.resource
+				};
+			}
+
+		} );
+	};
+
+	$scope.selectReportTerms = function( e, term ) {
+		var prop;
+
+		for( prop in $scope.reportTerms ) {
+			$scope.reportTerms[ prop ] = false;
+		}
+
+		$scope.reportTerms[ term ] = true;
+	};
+
+	$scope.loadAndInitPeople = function( ) {
+		var peopleInRoleQuery = {};
+
+		var peopleInRoleFields = {
+			resource: 1,
+			name: 1,
+			familyName: 1,
+			givenName: 1,
+			primaryRole: 1,
+			thumbnail: 1
+		};
+
+		Resources.query( 'people', peopleInRoleQuery, peopleInRoleFields, function( result ) {
+			$scope.peopleList = _.map( result.members, function( m ) {
+				return {
+					value: m.name,
+					resource: m.resource
+				};
+			} );
+
+			$( 'input[name="reportPerson"]' ).typeahead( {
+				minLength: 2,
+				highlight: true
+			}, {
+				templates: {
+					empty: 'No matching people'
+				},
+				name: 'peopleds',
+				source: Util.getTypeaheadStrFilter( $scope.peopleList )
+			} );
+		} );
+
+	};
+
+	$scope.loadAndInitProjectsAndClients = function( ) {
+		var query = {};
+
+		var fields = {
+			resource: 1,
+			name: 1,
+			familyName: 1,
+			givenName: 1,
+			primaryRole: 1,
+			thumbnail: 1
+		};
+
+		ProjectsService.getAllProjects( function( result ) {
+			$scope.projectList = _.map( result.data, function( m ) {
+				return {
+					value: m.name,
+					resource: m.resource
+				};
+			} );
+
+			$( 'input[name="reportProject"]' ).typeahead( {
+				minLength: 2,
+				highlight: true
+			}, {
+				templates: {
+					empty: 'No matching project'
+				},
+				name: 'projectds',
+				source: Util.getTypeaheadStrFilter( $scope.projectList )
+			} );
+
+			$scope.clientList = _.map( result.data, function( m ) {
+				return {
+					value: m.customerName
+
+				};
+			} );
+
+			$scope.clientList = _.filter( $scope.clientList, function( c ) {
+				return c.value;
+			} );
+			
+			$( 'input[name="reportClient"]' ).typeahead( {
+                minLength: 2,
+                highlight: true
+            }, {
+                templates: {
+                    empty: 'No matching client'
+                },
+                name: 'clientds',
+                source: Util.getTypeaheadStrFilter( $scope.clientList )
+            } );
+		} );
+
+	};
+
+	$scope.init = function( ) {
+		$scope.loadRoles( );
+		$scope.loadAndInitPeople( );
+		$scope.loadAndInitProjectsAndClients( );
+	};
+
+	$scope.selectReportType = function( e, report ) {
+		for( var prop in $scope.reportTypes ) {
+			$scope.reportTypes[ prop ] = false;
+		}
+
+		$scope.reportTypes[ report ] = true;
+	};
+
+	$scope.init( );
+} ] );
--- PSMasterMind/app/scripts/util/util.js
+++ PSMasterMind/app/scripts/util/util.js
@@ -2,5 +2,36 @@ var Util = {
 	alignDate: function( date ) {
 
 		return new Date( date.getFullYear( ), date.getMonth( ), date.getDate( ), 0, 0, 0 );
+	},
+
+	getTypeaheadStrFilter: function( strs ) {
+
+		return function findMatches( q, cb ) {
+			var matches, substringRegex;
+
+			// an array that will be populated with substring matches
+			matches = [ ];
+
+			// regex used to determine if a string contains the substring `q`
+			substrRegex = new RegExp( q, 'i' );
+
+			// iterate through the pool of strings and for any string that
+			// contains the substring `q`, add it to the `matches` array
+			$.each( strs, function( i, str ) {
+				if( str.value && substrRegex.test( str.value ) || substrRegex.test( str ) ) {
+					// the typeahead jQuery plugin expects suggestions to a
+					// JavaScript object, refer to typeahead docs for more info
+					if( str.value )
+						matches.push( str );
+					else
+						matches.push( {
+							value: str
+						} );
+				}
+			} );
+
+			cb( matches );
+		};
+
 	}
 };
