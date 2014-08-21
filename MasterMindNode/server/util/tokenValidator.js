e1811f31c6d45a8c1caefe76b164721a1b564d55
Vladimir Yancharuk <vladimir.yancharuk@pointsource.com>
2014-08-21 17:29:00 +0000

    Fixed auth

--- MasterMindNode/server/util/tokenValidator.js
+++ MasterMindNode/server/util/tokenValidator.js
@@ -1,25 +1,25 @@
 'use strict';
 
-var googleapis = require('googleapis');
-var config = require('../config/config.js');
+var googleapis = require( 'googleapis' );
+var config = require( '../config/config.js' );
 
-var validateGoogleToken = function(token, done){
-    googleapis
-    .discover('oauth2', 'v1')
-    .execute(function(err, client) {
-    client.oauth2.tokeninfo({ access_token: token })
-      .execute(function(err, result) {
-        if (err) {
-          console.log('Error occurred: ', err);
-        } else {
-          if(result.audience === config.google.clientID){
-            return done('', result.user_id);
-          } else {
-            return done('validation failed', null);
-          }
-        }
-      });
-    });
+var validateGoogleToken = function( token, done ) {
+	googleapis.discover( 'oauth2', 'v1' ).execute( function( err, client ) {
+		client.oauth2.tokeninfo( {
+			access_token: token
+		} ).execute( function( err, result ) {
+			if( err ) {
+				console.log( 'Error occurred: ', err );
+				return done( err, null );
+			} else {
+				if( result.audience === config.google.clientID && result.user_id ) {
+					return done( '', result.user_id );
+				} else {
+					return done( 'validation failed', null );
+				}
+			}
+		} );
+	} );
 };
 
 module.exports.validateGoogleToken = validateGoogleToken;
