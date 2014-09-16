18369fa92366297e903426120505d731b836bd1c
Daniil Dziaruhin <daniil.dziaruhin@pointsource.com>
2014-09-16 14:12:00 +0000

    53133 Implement notifications for vacations.
    nodemailer

--- MasterMindNode/server/util/emailSender.js
+++ MasterMindNode/server/util/emailSender.js
@@ -5,39 +5,50 @@
 var nodemailer = require('nodemailer');
 var smtpTransport = require('nodemailer-smtp-transport');
 
-/*var options = {
+var fs = require('fs');
+
+var privateKey  = fs.readFileSync('server/cert/server.key', 'utf8');
+var certificate = fs.readFileSync('server/cert/server.crt', 'utf8');
+
+var options = {
   from: "MasterMind Notice <system@pointsourcellc.com>",
   host: "smtp.gmail.com",
   //name: "pointsource.com",
   port: 465,
   auth: { 
-    user: "psapps@pointsourcellc.com",
-    pass: "ps@pp$777"
+    //user: "psapps@pointsourcellc.com",
+    //pass: "ps@pp$777"
+    user: "donila2008@gmail.com",
+    pass: "Nuttertools123"
   },
   //ignoreTLS: false,
   secure: true,
-
+  tls: {
+    key: privateKey,
+    cert: certificate
+  }
   //debug: true
-};*/
+};
 
-//var transporter = nodemailer.createTransport(smtpTransport(options));
+var transporter = nodemailer.createTransport(smtpTransport(options));
 
 var wellknown = require('nodemailer-wellknown');
 
 var config = wellknown("Gmail");
 console.log(config);
 
-var transporter = nodemailer.createTransport({
+/*var transporter = nodemailer.createTransport({
     service: 'Gmail',
     auth: {
       user: "psapps@pointsourcellc.com",
       pass: "ps@pp$777"
     }
-});
+});*/
 
 module.exports.sendEmailFromPsapps = function(to, subject, body, callback) {
   var mailOptions = { 
-    from: "MasterMind Notice <system@pointsourcellc.com>",
+    //from: "MasterMind Notice <system@pointsourcellc.com>",
+    from: "donila2008@gmail.com",
     to: to,
     subject: subject,
     html: body,
