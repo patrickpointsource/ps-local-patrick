735815c1cb3a29aaa5e3c1cd0ce2c35346259061
Anthony Segretto <anthony.segretto@pointsource.com>
2014-11-17 19:24:00 +0000

    test to deliver to multiple streams

--- MasterMindNode/scripts/deploystage.sh
+++ MasterMindNode/scripts/deploystage.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-
+#dist folder is for MasterMindNode and guidist is for PSMasterMind
 SOURCEDIR=/Users/anthonysegretto/Documents/workspace/MasterMindNode/dist/
 DESTINATIONDIR=stage.mm.pointsource.vpc:/home/anthonysegretto/dist/
 echo -e "\nDeploying mastermind build FROM:\n$SOURCEDIR\nTO:\n$DESTINATIONDIR"
