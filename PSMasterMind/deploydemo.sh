7eba2a22ad10e9e39d759af0cafa3158556b2846
anthonysegretto <anthonysegretto@pointsource.com>
2015-01-21 12:20:30 -0500

    github based deploy scripts

--- PSMasterMind/deploydemo.sh
+++ PSMasterMind/deploydemo.sh
@@ -1,7 +1,8 @@
 #/bin/bash
-#deploy MasterMindNode to dist - deploy PSMasterMind to guidist
-SOURCEDIR=/Users/anthonysegretto/Documents/workspace/PSMasterMind/dist/
-DESTINATIONDIR=demo.mm.pointsource.vpc:/home/anthonysegretto/dist/
+
+# MasterMindNode to demo
+SOURCEDIR=/Users/anthonysegretto/git/MasterMind/PSMasterMind/dist/
+DESTINATIONDIR=demo.mm.pointsource.vpc:/home/anthonysegretto/guidist/
 echo -e "\nDeploying mastermind build FROM:\n$SOURCEDIR\nTO:\n$DESTINATIONDIR"
 if [ -d "$DESTINATIONDIR" ]; then
   rm -r "$DESTINATIONDIR"
--- PSMasterMind/deploystage.sh
+++ PSMasterMind/deploystage.sh
@@ -1,6 +1,8 @@
 #/bin/bash
-SOURCEDIR=/Users/anthonysegretto/Documents/workspace/PSMasterMind/dist/
-DESTINATIONDIR=stage.mm.pointsource.vpc:/home/anthonysegretto/dist/
+
+# MasterMindNode to stage
+SOURCEDIR=/Users/anthonysegretto/git/MasterMind/PSMasterMind/dist/
+DESTINATIONDIR=stage.mm.pointsource.vpc:/home/anthonysegretto/guidist/
 echo -e "\nDeploying mastermind build FROM:\n$SOURCEDIR\nTO:\n$DESTINATIONDIR"
 if [ -d "$DESTINATIONDIR" ]; then
   rm -r "$DESTINATIONDIR"
