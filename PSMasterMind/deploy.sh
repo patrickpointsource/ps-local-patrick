74a303fd76e6556559cef5eb5de16f3463c153d1
anthonysegretto <anthonysegretto@pointsource.com>
2015-01-21 12:08:18 -0500

    remove old deploy scripts

--- PSMasterMind/deploy.sh
+++
@@ -1,9 +0,0 @@
-#/bin/bash
-SOURCEDIR=/Users/anthonysegretto/Documents/workspace/PSMasterMind/dist/
-DESTINATIONDIR=mastermind.pointsource.us:/home/anthonysegretto/dist/
-echo -e "\nDeploying mastermind build FROM:\n$SOURCEDIR\nTO:\n$DESTINATIONDIR"
-if [ -d "$DESTINATIONDIR" ]; then
-  rm -r "$DESTINATIONDIR"
-fi
-rsync -Crcv "$SOURCEDIR" "$DESTINATIONDIR"
-echo -e "\nDone"
