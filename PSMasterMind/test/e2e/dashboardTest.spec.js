93fccfddd5edd76ce089df6a52352f5d8f623c22
prakapchukv <vitali.prakapchuk@pointsource.com>
2015-02-03 19:49:29 +0300

    Dashboard E2E tests: Added logon double check.

--- PSMasterMind/test/e2e/dashboardTest.spec.js
+++ PSMasterMind/test/e2e/dashboardTest.spec.js
@@ -4,11 +4,21 @@
  * */
 describe('E2E: Dashboard Test Cases >', function() {
 	
+	var USER_NAME = 'psapps@pointsourcellc.com';
+	var PASSWORD = 'ps@pp$777';
+	
 	var HOURS_PROJECT = "MasterMind";
 	var HOURS_VALUE = 8;
 	var HOURS_ABSURD_VALUE = 50;
 	var HOURS_NULL_VALUE = 0;
 	var HOURS_DESCRIPTION = "Hours Widget: E2E Testing";
+	
+	//login
+	var sbutton = by.tagName('button');
+	var logonEmail = by.id('Email');
+	var logonPswd = by.id('Passwd');
+	var signIn = by.id('signIn');
+	var submit_approve_access = by.id('submit_approve_access');
 	
 	//Hours widget
 	var loggedProject = 'loggedProject';
@@ -31,6 +41,14 @@ describe('E2E: Dashboard Test Cases >', function() {
 	var pipelineProjectCount = 'pipelineProjectCount';
 	var investmentProjectCount = 'investmentProjectCount';
 	
+
+	beforeEach(function() {
+		browser.driver.getCurrentUrl().then(function(url) {
+			if ( url.indexOf('http://localhost:9000/index.html#/') == -1 ) {
+				login();
+			}
+		});
+	});
 	
 	var dashboardHoursWidgetAddRemoveRecordTest = function () {
 		browser.wait(function(){	    		
@@ -135,9 +153,9 @@ describe('E2E: Dashboard Test Cases >', function() {
 		console.log("> Adding hours record.");
 		browser.findElement(byId(loggedProjectInput)).sendKeys(HOURS_PROJECT);
 		browser.wait(function(){	    		
-    		return browser.isElementPresent(by.id(ddlProjectsTasks));
+    		return browser.isElementPresent(byId(ddlProjectsTasks));
     	}).then(function(){
-    		browser.findElement(by.id(ddlProjectsTasks)).click();
+    		browser.findElement(byId(ddlProjectsTasks)).click();
     		browser.findElement(byId(loggedHoursInput)).sendKeys(hours);
     		browser.findElement(byId(loggedDescriptionInput)).sendKeys(HOURS_DESCRIPTION);
     		browser.findElement(byId(hoursAdd)).click();
@@ -149,14 +167,58 @@ describe('E2E: Dashboard Test Cases >', function() {
 		return index ? by.id(id + index) : by.id(id + '0');
 	};
 	
-	beforeEach(function() {
-		browser.getCurrentUrl().then(function(url) {
-			if ( url.indexOf('http://localhost:9000/index.html#/') == -1 ) {
-				browser.get('http://localhost:9000/index.html#/');
-				browser.sleep(1000);
-			}
-		});
-	});
+	var login = function () {
+		browser.driver.ignoreSynchronization = true;
+	    browser.driver.get('http://localhost:9000');
+	    
+	    browser.driver.wait(function() {	    	
+	    	return browser.driver.isElementPresent(sbutton);
+	    	
+	    }).then(function(){
+		    // expect the signin button to be present
+	    	// expect(browser.driver.isElementPresent(sbutton)).toBeTruthy();
+		    console.log('login button is available. Clicking it');
+	    	// find the signin button and click it
+		    browser.driver.findElement(sbutton).click();		    
+
+	    }); 
+
+	    // expect the popup window to open and check that its url contains accounts.google.com
+	    browser.driver.getAllWindowHandles().then(function (handles) {		    	
+	    	browser.driver.switchTo().window(handles[1]).then(function(){
+	    		console.log("> Swicthed window control to the popup.");
+	    	});
+	    	
+	    	expect(browser.driver.getCurrentUrl()).toContain('https://accounts.google.com/ServiceLogin?');
+	    	
+	    	browser.driver.wait(function(){	    		
+	    		return browser.driver.isElementPresent(logonEmail);
+	    	}).then(function(){
+	    		console.log("> Input fields found. Populating and submitting");
+	    		browser.driver.findElement(logonEmail).sendKeys(USER_NAME);
+	    		browser.driver.findElement(logonPswd).sendKeys(PASSWORD);
+	    		browser.driver.findElement(signIn).click();	   
+	    		browser.driver.sleep(2000);	
+	    		
+	    		// At this moment the accept window might be closed. 
+	    		// So, check the total amount of windows again. If it is more than one,
+	    		// goahead and click the accept button, otherwise do nothing.
+	    		browser.driver.getAllWindowHandles().then(function (handles) {
+	    			
+	    			if(handles.length > 1){
+	    	    		browser.driver.findElement(submit_approve_access).click();    		    			
+	    			}   			
+	    			
+	    		});
+	    		
+	    		// back to the main window
+				browser.driver.switchTo().window(handles[0]);
+	    		browser.driver.sleep(5000);	    		
+	    	});
+	    	
+	    });
+	};
+	
 	
 	it('Hours Widget Test: Add\Remove record.', function() {	
 		console.log('> Running: Hours Widget - Add\Remove record.');
