5fd5ecd4ab13a883fbf26750e1ba9889beb4c5e1
Daniil Dziaruhin <daniil.dziaruhin@pointsource.com>
2015-02-19 20:17:43 +0300

    PSMM-32: Fixed index.html, added ooo controller for ooo widget, separated with calendar controller.

--- PSMasterMind/app/modules/calendar/controllers/calendar.js
+++ PSMasterMind/app/modules/calendar/controllers/calendar.js
@@ -8,50 +8,6 @@ angular.module('Mastermind').controller('CalendarCtrl', [
     '$scope', '$state', '$filter', '$q', 'VacationsService',
     'Resources', 'ngTableParams', 'VacationsService', 
     function($scope, $state, $filter, $q, VacationsService, Resources, TableParams) {
-
-        $scope.initOOO = function() {
-            Resources.refresh("vacations/my").then(function(result) {
-                if (result) {
-                    $scope.oooPeriods = result.periods;
-                    $scope.oooDaysLeft = result.daysLeft;
-                }
-            });
-        };
-
-        $scope.initOOO();
-
-        $scope.editableVacation = null;
-        $scope.collapsedPeriodIndex = 0;
-
-        $scope.collapsePeriod = function(index) {
-            if ($scope.collapsedPeriodIndex == index) {
-                $scope.collapsedPeriodIndex = -1;
-            } else {
-                $scope.collapsedPeriodIndex = index;
-            }
-        };
-        
-        $scope.getShortDate = function(date) {
-            return moment(date).format("MMM DD");
-        };
-        
-        $scope.getDays = function(vacation) {
-            return VacationsService.getDays(vacation.startDate, vacation.endDate);
-        };
-        
-        $scope.getStatusText = function(status) {
-            return VacationsService.getStatusText(status);
-        };
-        
-        $scope.getTotalDays = function(period) {
-            var hours = 0;
-            _.each(period.vacations, function(vacation) {
-                hours += VacationsService.getHoursLost(vacation);
-            });
-            
-            return (hours / 8).toFixed(1);
-        }
-        
         $scope.startDate = '';
     	$scope.endDate = "";
     	$scope.months = [ 'Janurary', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December' ];
@@ -60,26 +16,7 @@ angular.module('Mastermind').controller('CalendarCtrl', [
 
         $scope.moment = window.moment ? window.moment: moment;
         $scope.currentMonth = $scope.moment();
-        $scope.editableVacation = null;
-        $scope.newVacationCreation = false;
         
-        $scope.requestNewVacation = function() {
-            $scope.newVacationCreation = true;
-        }
-        
-        $scope.editManager = false;
-        
-        $scope.editManagerCallback = function() {
-            $scope.editManager = true;
-            setTimeout(function() { 
-                $(".select-vacation-manager").selectpicker();
-            }, 5);
-        }
-        
-        $scope.managerSelected = function() {
-            $scope.vacationManager = this.vacationManager;
-            $scope.editManager = false;
-        }
         /**
          * Go back
          */
