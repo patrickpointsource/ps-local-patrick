827839872691d60caae190fa06b09b03690474ab
rubo <ruben@buniatyan.com>
2015-03-05 00:47:02 +0100

    [PSMM-112] New Reporting: People Report -> Utilization by Role bar graph -> Legend changes

--- PSMasterMind/app/modules/reports/controllers/pieChart.js
+++ PSMasterMind/app/modules/reports/controllers/pieChart.js
@@ -4,108 +4,198 @@
  * Controller for people report.
  */
 
-angular.module( 'Mastermind.controllers.reports' ).controller( 'PieChartCtrl', [ '$scope', '$q', '$state', '$stateParams', '$filter', '$location', 'Resources', 
-function( $scope, $q, $state, $stateParams, $filter, $location, Resources) {
-  $scope.elemId = null;
- 
-  $scope.render = function(elId) {
-	  var el = $('#' + elId + ' div');
-	  
-	  el = $('<div class="d3Div" style="height:100%" id="d3Div' + elId + '"><svg style="font-size: 11px;height:100%;" ' + 
-		' xmlns="http://www.w3.org/2000/svg"></svg></div>').appendTo(el);
-	
-	 
-	 //var axisLabels = ["People", "Count"]
-	 
-	 var hideLegend = true;
+angular.module('Mastermind.controllers.reports').controller('PieChartCtrl', ['$scope', '$q', '$state', '$stateParams', '$filter', '$location', 'Resources',
+function ($scope, $q, $state, $stateParams, $filter, $location, Resources) {
 
-	var preparedData = $scope.chartData;
-	var allEmpty = true;
-	
-	for (var k = 0 ;  preparedData && k < preparedData.length;  k++) {
-		allEmpty = allEmpty && preparedData[k].value == 0;
-		
-		if (preparedData[k].value > 0)
-			preparedData[k].value = (parseFloat(preparedData[k].value)).toFixed(1);
-	}
-	
-	var colors = ['#00BBD6', '#5BC9D1', '#92D7E7', '#D0ECF4', '#97D7D9', '#4EC2C7'];
-	
-	
-	 var colorFn = d3.scale.category10(); 
-	 //var colors = []
-	
-	 //for (var k = 0; k < preparedData.length; k ++)
-	//		colors.push(colorFn(k))
-	
-	 if (!allEmpty)
-		 nv.addGraph(function() {
-			try {
-				var chart = nv.models.pieChart()
-	        	.x(function(d) { 
-			 		//return d.value;
-			 		return d.key;
-        		})
-		        .y(function(d) { return d.value; })
-		        .showLabels(true)
-		        .labelThreshold(0.02)
-		        .color(colors)
-		        .height(300);
-				//.donut(true)
-		 		//.values(function(d) { return d }) // Error occurred
+    $scope.output = {};
+    $scope.el = null;
+    $scope.elemId = null;
 
-				
-				//chart.showLegend(false);
-				
-				//chart.showControls(false);
-				
-		 		nv.models.legend().width(320);
-	 			
-				var pie = chart.pie;
-				
-				 // override tooltip positioning function to fix top tooltip position
-				pie.dispatch.on('elementMouseover.tooltip', function(e) {
-					e.pos = [e.pos[0], e.pos[1]];
-					chart.dispatch.tooltipShow(e);
-				});
-				
-		 		d3.select('#' + el.attr('id') + ' svg')
-	          		.datum(preparedData)
-	          		.attr('class', 'chart')
-			        .transition().duration(1000)
-			        .call(chart);
-				 
-		 		var legendEl = $('#' + el.attr('id') + ' .nv-legendWrap').get(0);
-		 		var legendSize = legendEl && legendEl.getBoundingClientRect ? legendEl.getBoundingClientRect(): {
-		 			height: 0,
-		 			width: 0
-		 		};
-		 		
-		 		chart.update();
-		 		/*
-		 		// apply styles which will be only actual when chart is exported
-				el.find('g.nv-label > rect').css('fill', 'transparent');
-				el.find('g.nv-label > rect').css('stroke', 'transparent');
-				el.find('g.nv-label > text').css('stroke', '#000');
-				*/
-				nv.utils.windowResize(chart.update);
-				//$('#' + el.attr('id')).data('chart', chart);
-				
-				return chart;
-			} catch(ex) {
-				el.html('Error occured while rendering')
-			}
-		});
-	 else
-		 el.html('<br/><br/><br/><b>Empty data loaded</b>');
+    $scope.render = function (elId) {
+        var el = $('#' + elId + ' div');
 
-  };
-  
-  setTimeout(function() {
-	  var id = $scope.$parent.elemId ? $scope.$parent.elemId: $scope.$parent.$parent.elemId;
-	  
-	  $scope.render(id);
-  }, 1 * 1000);
+        el = $('<div id="' + elId + 'chartContainer" ></div>').appendTo(el);
 
-  
-} ] );
+        var chartData = $scope.chartData;
+        var width = $scope.width ? $scope.width : 400;
+        var height = $scope.height ? $scope.height : 250;
+        var svg = dimple.newSvg("#" + elId + "chartContainer", width, height + 50);
+        var legendBoxHeight = height * .65;
+
+        var g = svg.append("g")
+            .attr("x", width / 2 + 20)
+            .attr("y", (height - legendBoxHeight) / 2)
+            .attr("width", width / 2)
+            .attr("height", height * .75)
+            .attr("transform", "translate(" + (width / 2 + 20) + ", " + ((height - legendBoxHeight) / 2) + ")");
+
+        g.append("rect")
+            .attr("x", 0)
+            .attr("y", 0)
+            .attr("width", width / 2)
+            .attr("height", legendBoxHeight)
+            .attr("stroke", "#CCCCCC")
+            .attr("stroke-width", 1)
+            .attr("fill-opacity", 0);
+
+        g.append("text")
+            .attr("x", 7)
+            .attr("y", 15)
+            .attr("width", 100)
+            .attr("height", 22)
+            .style("font-size", "10px")
+            .style("font-weight", "bold")
+            .text("Key");
+
+        // Create the chart
+        var myChart = new dimple.chart(svg, chartData);
+        myChart.setBounds(20, 25, width / 2 - 20, height - 50);
+        // Add an x and 3 y-axes.  When using multiple axes it's
+        // important to assign them to variables to pass to the series
+        myChart.addMeasureAxis("p", "value");
+
+        // Add the bars mapped to the second y axis
+        myChart.addSeries("key", dimple.plot.pie);
+        //configBarTooltip(svg, bar, xTitle, yTitle);
+
+        myChart.draw();
+
+        var myLegend = myChart.addLegend(width / 2, (height - legendBoxHeight) / 2 + 30, width / 2 - 40, height / 2 - 60, "right");
+
+        myChart.draw();
+
+        myChart.legends = [];
+
+        // This block simply adds the legend title. I put it into a d3 data
+        // object to split it onto 2 lines.  This technique works with any
+        // number of lines, it isn't dimple specific.
+        svg.selectAll("title_text")
+            .data(["Click legend to show/hide hours."])
+            .enter()
+            .append("text")
+            .attr("x", width / 2 + 30)
+            .attr("y", function (d, i) { return height / 2 + legendBoxHeight / 2 - 7; })
+            .style("font-family", "sans-serif")
+            .style("font-size", "10px")
+            .style("font-weight", "normal")
+            .text(function (d) { return d; });
+
+        // Get a unique list of Owner values to use when filtering
+        var filterValues = dimple.getUniqueValues(chartData, "key");
+        // Get all the rectangles from our now orphaned legend
+        myLegend.shapes.selectAll("rect")
+          // Add a click event to each rectangle
+          .on("click", function (e) {
+              // This indicates whether the item is already visible or not
+              var hide = false;
+              var newFilters = [];
+              // If the filters contain the clicked shape hide it
+              filterValues.forEach(function (f) {
+                  if (f === e.aggField.slice(-1)[0]) {
+                      hide = true;
+                  } else {
+                      newFilters.push(f);
+                  }
+              });
+              // Hide the shape or show it
+              if (hide) {
+                  d3.select(this).style("opacity", 0.2);
+              } else {
+                  newFilters.push(e.aggField.slice(-1)[0]);
+                  d3.select(this).style("opacity", 0.8);
+              }
+              // Update the filters
+              filterValues = newFilters;
+              // Filter the data
+              myChart.data = dimple.filterData(chartData, "key", filterValues);
+              // Passing a duration parameter makes the chart animate. Without
+              // it there is no transition
+              myChart.draw(800);
+          });
+
+    };
+
+    var configBarTooltip = function (svg, bar, xTitle, yTitle) {
+        var popup;
+        bar.addEventHandler("mouseover", onBarHover);
+        bar.addEventHandler("mouseleave", onBarLeave);
+
+        function onBarHover(e) {
+
+            if (!e.yValue || e.yValue == 0)
+                return;
+
+            // Get the properties of the selected shape
+            var cx = parseFloat(e.selectedShape.attr("x")),
+        		cy = parseFloat(e.selectedShape.attr("y")),
+        		cwidth = parseFloat(e.selectedShape.attr("width")),
+        		cheight = parseFloat(e.selectedShape.attr("height"));
+            var fill = e.selectedShape.attr("fill");
+            var stroke = e.selectedShape.attr("stroke");
+
+            // Set the size and position of the popup
+            var width = 150,
+				height = 55,
+				x = (cx + 2 * width < svg.attr("width") ? cx + cwidth : cx - width - 10),
+        		y = (cy - height < height ? height : cy - height);
+
+            // Create a group for the popup
+            popup = svg.append("g");
+
+            // Add a rectangle surrounding the tooltip content
+            popup
+        		.append("rect")
+        		.attr("x", x + 5)
+        		.attr("y", y - 5)
+        		.attr("width", width)
+        		.attr("height", height)
+        		.attr("rx", 5)
+        		.attr("ry", 5)
+        		.attr("fill-opacity", 0.7)
+        		.style("fill", fill)
+        		.style("stroke", stroke)
+        		.style("stroke-width", 2);
+
+            // Add custom tooltip content
+            popup
+        		.append('text')
+        		.style("font-family", "sans-serif")
+        		.style("font-size", 10)
+        		.attr('x', x + 10)
+        		.attr('y', y + 5)
+        		.append('tspan')
+        		.attr('x', x + 10)
+        		.attr('y', y + 10)
+        		.text(e.seriesValue[0])
+        		.append('tspan')
+        		.attr('x', x + 10)
+        		.attr('y', y + 25)
+        		.text(capitalizeString(xTitle) + ": " + e.xValue)
+        		.append('tspan')
+        		.attr('x', x + 10)
+        		.attr('y', y + 40)
+        		.text(capitalizeString(yTitle) + ": " + e.yValue);
+        };
+
+        function onBarLeave(e) {
+            if (popup && popup.remove()) {
+                popup.remove();
+            }
+        };
+
+    };
+
+    setTimeout(function () {
+        var id = $scope.$parent.elemId ? $scope.$parent.elemId : $scope.$parent.$parent.elemId;
+
+        $scope.render(id);
+    }, 1 * 1000);
+
+    var capitalizeString = function (str) {
+        if (str && str.length > 1) {
+            return str.charAt(0).toUpperCase() + str.slice(1);
+        } else {
+            return str ? str.charAt(0).toUpperCase() : '';
+        }
+    };
+}]);
