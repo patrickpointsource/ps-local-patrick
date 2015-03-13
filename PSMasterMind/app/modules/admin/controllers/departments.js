1ece9454c3db44417b02254a399b3de74c4ab745
vyancharuk <vladimir.yancharuk@pointsourcellc.com>
2015-03-13 19:20:33 +0300

    PSMM-261 implement add/edit department UI panel .PSMM-259 add department tab to administration area, with  list of available departments with all add/edit/delete and search input UI controls .

--- PSMasterMind/app/modules/admin/controllers/departments.js
+++ PSMasterMind/app/modules/admin/controllers/departments.js
@@ -19,6 +19,7 @@ angular.module('Mastermind')
 	  };
 	  
 	  $scope.cancelDepartment = function () {
+		  $scope.selectedDepartment = {};
 		  //Clear New Task Form
 		  $scope.selectedDepartmentForm.$setPristine();
 	  };
@@ -26,8 +27,70 @@ angular.module('Mastermind')
 	  $scope.$on("admin:departments", function(event, command) {
 		  if (command == 'create') {
 			  $scope.selectedDepartment = {isNew: true};
+			  $scope.selectedDepartmentPeople = [];
 		  }
 	  });
+	  
+	  $scope.selectDepartment = function(e, department) {
+		  $scope.selectedDepartment = department;
+		  
+		  $scope.selectedDepartmentPeople = [];
+		  var found;
+		  
+		  for (var k = 0; $scope.selectedDepartment.departmentPeople && k < $scope.selectedDepartment.departmentPeople.length; k ++) {
+			  found = _.find($scope.availablePeopleList, function(p){ return p.resource == $scope.selectedDepartment.departmentPeople[k]});
+			  $scope.selectedDepartmentPeople.push({
+				  resource: $scope.selectedDepartment.departmentPeople[k],
+				  name: found ? found.name: ''
+			  });
+		  }
+			  
+	  };
+	  
+	  $scope.editDepartmentPeople = function() {
+		  //$scope.selectedDepartmentPeople = [];
+		  $scope.selectedDepartment.editDepartmenPeople = true;
+	  };
+	  
+	  $scope.cancelDepartmentPeople = function() {
+		  $scope.selectedDepartment.editDepartmenPeople = false;
+		  
+		  var ind;
+		  
+		  for (var k = 0; k < $scope.selectedDepartmentPeople.length; k ++) {
+			  ind = _.indexOf($scope.selectedDepartment.departmentPeople, $scope.selectedDepartmentPeople[k].resource);
+			  
+			  if (ind >= 0)
+				  $scope.selectedDepartment.departmentPeople.splice(ind, 1);
+		  }
+	  };
+	  
+	  $scope.saveDepartmentPeople = function() {
+		  $scope.selectedDepartment.editDepartmenPeople = false;
+		  
+		  $scope.selectedDepartment.departmentPeople = [];
+		  for (var k = 0; k < $scope.selectedDepartmentPeople.length; k ++)
+			  $scope.selectedDepartment.departmentPeople.push($scope.selectedDepartmentPeople[k].resource);
+		  
+		  $scope.saveDepartment();
+	  };
+	  
+	  $scope.addDepartmentPerson = function(p) {
+		  if (! $scope.selectedDepartment.departmentPeople)
+			  $scope.selectedDepartment.departmentPeople = [];
+		  
+		  $scope.selectedDepartment.departmentPeople.push(p.resource);
+		  
+		  $scope.selectedDepartment.departmentPeople = _.uniq( $scope.selectedDepartment.departmentPeople);
+		  
+		  $scope.selectedDepartmentPeople.push(p);
+		  
+		  p.added = true;
+	  };
+	  
+	  $scope.removeDepartmentPerson = function(person) {
+		  $scope.selectedDepartmentPeople = _.filter($scope.selectedDepartmentPeople, function(p){ return p.resource != person.resource});
+	  };
 	    /**
 	     * Add a new task to the server
 	     */
@@ -84,7 +147,9 @@ angular.module('Mastermind')
 
 	  
 	    $scope.loadDepartments = function() {
-	    	return DepartmentsService.refreshDepartments();
+	    	return DepartmentsService.refreshDepartments().then(function(result) {
+	    		$scope.availableDepartments = result;
+	    	});
 	    };
 	    
 	    $scope.initDepartments =  function () {
@@ -123,7 +188,7 @@ angular.module('Mastermind')
 	        			
 	        		});
 	 	        	
-	        		$scope.availablePeopleList.splice(0, $scope.availablePeopleList.length - 7);
+	        		//$scope.availablePeopleList.splice(0, $scope.availablePeopleList.length - 7);
 	 	        		
 	 	        });
 	    	
