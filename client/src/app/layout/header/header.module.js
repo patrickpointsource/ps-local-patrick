b098dc1559a81127541ca119066ae6e2dca21713
Robbie McAlister <Robbie.McAlister@pointsource.com>
2015-05-11 16:22:05 -0400

    Bunch of changes for the menu

--- client/src/app/layout/header/header.module.js
+++ client/src/app/layout/header/header.module.js
@@ -1,9 +1,30 @@
 /* Copyright © 2015 PointSource, LLC. All rights reserved. */
-(function() {
-  'use strict';
+(function () {
+    'use strict';
 
-  angular
-      .module('mastermind.layout.header', []);
+    angular
+        .module('mastermind.layout.header', [])
+        .directive('headerBar', function () {
+            // Runs during compile
+            return {
+                name: 'headerBar',
+                // priority: 1,
+                // terminal: true,
+                // scope: {}, // {} = isolate, true = child, false/undefined = no change
+                // controller: function($scope, $element, $attrs, $transclude) {},
+                // require: 'ngModel', // Array = multiple requires, ? = optional, ^ = check parent elements
+                // restrict: 'A', // E = Element, A = Attribute, C = Class, M = Comment
+                // template: '',
+                templateUrl: 'app/layout/header/header.html',
+                replace: true,
+                // transclude: true,
+                // compile: function(tElement, tAttrs, function transclude(function(scope, cloneLinkingFn){ return
+                // function linking(scope, elm, attrs){}})),
+                link: function ($scope, iElm, iAttrs, controller) {
+
+                }
+            };
+        });
 
 
 })();
