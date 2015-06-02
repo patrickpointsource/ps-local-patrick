86b25fc72c271f936115cd8d687b9277d1ce41bf
ps-dino <denis.novalenko@pointsource.com>
2015-06-02 21:42:09 +0300

    fixed date timezone issues

--- server/test/rest/Assignments.js
+++ server/test/rest/Assignments.js
@@ -137,7 +137,7 @@ describe('ASSIGNMENTS - test simple REST calls', function () {
             assert.equal(json.role, DEFAULT_ROLE.role);
             assert.equal(json.person, DEFAULT_ROLE.person);
             assert.equal(json.project, DEFAULT_ROLE.project);
-            assert.equal(moment(json.startDate).format('YYYY-MM-DD'), DEFAULT_ROLE.startDate);
+            assert.equal(moment(json.startDate, 'YYYY-MM-DD HH:mm Z').format('YYYY-MM-DD'), DEFAULT_ROLE.startDate);
              
             // Save the assignmentID to do an update and delete later
             assignmentID = json.id;
@@ -216,7 +216,7 @@ describe('ASSIGNMENTS - test simple REST calls', function () {
             assert.equal(json.role, DEFAULT_ROLE.role);
             assert.equal(json.person, DEFAULT_ROLE.person);
             assert.equal(json.project, DEFAULT_ROLE.project);
-            assert.equal(moment(json.startDate).format('YYYY-MM-DD'), DEFAULT_ROLE.startDate);
+            assert.equal(moment(json.startDate, 'YYYY-MM-DD HH:mm Z').format('YYYY-MM-DD'), DEFAULT_ROLE.startDate);
             done();
         });
     });
@@ -316,8 +316,7 @@ describe('ASSIGNMENTS - test simple REST calls', function () {
             assert.equal(json.role, CHANGED_ROLE.role);
             assert.equal(json.person, CHANGED_ROLE.person);
             assert.equal(json.project, CHANGED_ROLE.project);
-            assert.equal(moment(json.startDate).format('YYYY-MM-DD'), CHANGED_ROLE.startDate);
-
+            assert.equal(moment(json.startDate, 'YYYY-MM-DD HH:mm Z').format('YYYY-MM-DD'), CHANGED_ROLE.startDate);
             done();
         });
     });
