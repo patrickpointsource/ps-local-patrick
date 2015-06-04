c4346fd47ec3e510ef9adbe1a3f4cc62873f7c52
Robbie McAlister <Robbie.McAlister@pointsource.com>
2015-06-04 11:22:04 -0400

    Pulling out some libraries that aren't being used.

--- client/src/app/app.module.js
+++ client/src/app/app.module.js
@@ -10,11 +10,9 @@
         .module('mastermind', [
             'app.config',
             'app.services',
-
             'ui.router',
             'ngAnimate',
             'ngTouch',
-
             'foundation',
             'restangular',
             'ngCordova',
@@ -174,12 +172,9 @@
                 AuthService.refreshAccessToken().then(function() {
                     // Repeat the request and then call the handlers the usual way.
                     $http(response.config).then(responseHandler, deferred.reject);
-                    // Be aware that no request interceptors are called this way.
                 });
-
                 return false; // error handled
             }
-
             return true; // error not handled
         });
 
--- client/src/app/modules/people/people.controller.js
+++ client/src/app/modules/people/people.controller.js
@@ -6,16 +6,74 @@
         .module('PeopleModule')
         .controller('PeopleController', PeopleController);
 
-    PeopleController.$inject = ['psafLogger', 'PeopleService'];
+    PeopleController.$inject = ['psafLogger', 'PeopleService', 'RolesService', 'AssignmentsService'];
 
-    function PeopleController(psafLogger, PeopleService) {
+    function PeopleController(psafLogger, PeopleService, RolesService, AssignmentsService) {
         var people = this;
         var logs = psafLogger.getInstance('mastermind');
-        people.athing = 'athing';
 
-        PeopleService.getPeople().then(function(response) {
-            people.list = response;
+        RolesService.getRoles().then(function(response) {
+            var roles = response;
+            people.roles = {};
+            angular.forEach(roles, function(role) {
+                if (!role.abbreviation) {
+                    role.abbreviation = 'admin';
+                }
+                people.roles[role.id] = role;
+            });
+
+            PeopleService.getPeople().then(function(response) {
+                people.list = response;
+                angular.forEach(people.list, function(person) {
+                    person.role = people.roles[person.primaryRole];
+                    person.utilization = Math.floor(Math.random() * 100);
+                });
+            });
         });
+
+        var currentAssignments = [];
+
+        AssignmentsService.getAssignments().then(function(response) {
+            angular.forEach(response, function(assignment) {
+                if (assignment.isCurrent) {
+                    currentAssignments.push(assignment);
+                }
+            });
+            console.log('Assignments ---->', currentAssignments);
+        });
+
+        people.sorter = 'name.familyName';
+        people.sortReverse = false;
+        people.sorting = '';
+
+        people.sortFilter = function(sortParam) {
+            switch(sortParam) {
+                case 'name':
+                    people.sorter = 'name.familyName';
+                    people.sortReverse = false;
+                    break;
+                case 'name-desc':
+                    people.sorter = 'name.familyName';
+                    people.sortReverse = true;
+                    break;
+                case 'role':
+                    people.sorter = 'role.name';
+                    people.sortReverse = false;
+                    break;
+                case 'utilization':
+                    people.sorter = 'utilization';
+                    people.sortReverse = false;
+                    break;
+                case 'utilization-desc':
+                    people.sorter = 'utilization';
+                    people.sortReverse = true;
+                    break;
+                default:
+                    people.sorter = 'name.familyName';
+                    people.sortReverse = false;
+            }
+        };
     }
+
 
 })();
